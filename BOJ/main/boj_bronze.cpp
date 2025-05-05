b2f82de0785e7220c58bb0d53077b1d65c881e08
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-05-06 00:27:02 +0900

    25

--- BOJ/main/boj_bronze.cpp
+++ BOJ/main/boj_bronze.cpp
@@ -9,16 +9,17 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 
-int A, B, C, D;
+int N, M, K;
+ll a, b, C, T;
+std::vector<ll> V;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> A >> B;
-	C = A + B;
-	D = A - B;
-	if (C < D) std::swap(C, D);
-	std::cout << C << "\n" << D << "\n";
+	V.resize(4);
+	for (int i = 0; i < 4; i++) std::cin >> V[i];
+	std::sort(V.begin(), V.end());
+	std::cout << (V[1] + V[2] + V[3] + 1) << "\n";
 	return 0;
 }

d0b701512b27c20ad5e4756100e7b00cb0ce8426
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-05-19 23:50:36 +0900

    25

--- BOJ/main/boj_bronze.cpp
+++ BOJ/main/boj_bronze.cpp
@@ -18,11 +18,22 @@ int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cin >> T;
-	while (T--) {
+	//while (T--) {
+	//	std::cin >> N;
+	//	while (N--) {
+	//		std::cin >> A >> B;
+	//		std::cout << A + B << " " << A * B << "\n";
+	//	}
+	//}
+	for (int i = 1; i <= T; i++) {
 		std::cin >> N;
-		while (N--) {
-			std::cin >> A >> B;
-			std::cout << A + B << " " << A * B << "\n";
+		std::cout << "Case #" << i << ": ";
+		if (N <= 25) std::cout << "World Finals\n";
+		else {
+			std::cout << "Round ";
+			if (N <= 1000) std::cout << "3\n";
+			else if (N <= 4500) std::cout << "2\n";
+			else std::cout << "1\n";
 		}
 	}
 	return 0;

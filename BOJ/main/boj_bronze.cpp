6a5c02516ce4de7f16fbc9820a3a3c723ba7d273
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-05-20 00:06:23 +0900

    25

--- BOJ/main/boj_bronze.cpp
+++ BOJ/main/boj_bronze.cpp
@@ -14,10 +14,11 @@ typedef std::vector<ld> Vld;
 
 int T, L, N, H, C, A, B, D;
 std::string S;
+int I[10];
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> T;
+	//std::cin >> T;
 	//while (T--) {
 	//	std::cin >> N;
 	//	while (N--) {
@@ -25,15 +26,18 @@ int main() {
 	//		std::cout << A + B << " " << A * B << "\n";
 	//	}
 	//}
-	for (int i = 1; i <= T; i++) {
-		std::cin >> N;
-		std::cout << "Case #" << i << ": ";
-		if (N <= 25) std::cout << "World Finals\n";
-		else {
-			std::cout << "Round ";
-			if (N <= 1000) std::cout << "3\n";
-			else if (N <= 4500) std::cout << "2\n";
-			else std::cout << "1\n";
+	for (int i = 0; i < 10; i++) {
+		std::cin >> I[i];
+	}
+	for (int i = 0; i < 10; i++) {
+		T = 0;
+		for (int j = 0; j < 10; j++) {
+			if (i == j) continue;
+			T += I[j];
+		}
+		if (T == I[i]) {
+			std::cout << I[i] << "\n";
+			break;
 		}
 	}
 	return 0;

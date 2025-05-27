da2ffa9a35daece3efc2939f522a3e6deb02debb
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-05-28 00:51:42 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -29,23 +29,43 @@ int N;
 struct Pos {
 	ld x, y;
 	Pos(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) {}
+	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
 	Pos operator * (const ld& n) const { return { x * n, y * n }; }
+	Pos operator / (const ld& n) const { return { x / n, y / n }; }
 	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
 	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos rot(const ld& t) const { return Pos(x * cosl(t) - y * sinl(t), x * sinl(t) + y * cosl(t)); }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const ld& n) { x *= n; y *= n; return *this; }
+	Pos& operator /= (const ld& n) { x /= n; y /= n; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ld xy() const { return x * y; }
+	Pos rot(const ld& t) const { return { x * cos(t) - y * sin(t), x * sin(t) + y * cos(t) }; }
 	ld Euc() const { return x * x + y * y; }
-	ld mag() const { return sqrtl(Euc()); }
+	ld mag() const { return sqrt(Euc()); }
 	Pos unit() const { return *this / mag(); }
-	ld rad() const { return atan2l(y, x); }
+	ld rad() const { return atan2(y, x); }
 	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
+	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
+	bool close(const Pos& p) const { return zero((*this - p).Euc()); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = { 0, 0 };
 typedef std::vector<Pos> Polygon;
 ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
 ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3) / (d2 - d1).mag(); }
+Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) { ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2); return (p1 * a2 + p2 * a1) / (a1 + a2); }
 ld dist(const Pos& d1, const Pos& d2, const Pos& t, bool f = 0) {
 	if (!f) return cross(d1, d2, t) / (d1 - d2).mag();
 	if (sign(dot(d1, d2, t)) <= 0 &&
@@ -63,9 +83,48 @@ void norm(Polygon& H) {
 	ld A = area(H); if (A < 0) std::reverse(H.begin(), H.end());
 	return;
 }
+Polygon polygon_cut(const Polygon& ps, const Pos& b1, const Pos& b2) {
+	Polygon qs;
+	int n = ps.size();
+	for (int i = 0; i < n; i++) {
+		Pos p1 = ps[i], p2 = ps[(i + 1) % n];
+		int d1 = ccw(b1, b2, p1), d2 = ccw(b1, b2, p2);
+		if (d1 >= 0) qs.push_back(p1);
+		if (d1 * d2 < 0) qs.push_back(intersection(p1, p2, b1, b2));
+	}
+	return qs;
+}
+Polygon sutherland_hodgman(const Polygon& C, const Polygon& clip) {
+	int sz = clip.size();
+	std::vector<Pos> ret = C;
+	for (int i = 0; i < sz; i++) {
+		Pos b1 = clip[i], b2 = clip[(i + 1) % sz];
+		ret = polygon_cut(ret, b1, b2);
+	}
+	return ret;
+}
 struct Seg {
 	Pos s, e, dir;
 	Seg(Pos s_ = Pos(), Pos e_ = Pos()) : s(s_), e(e_) { dir = e - s; }
+	//bool operator < (const Seg& l) const { return s == l.s ? e < l.e : s < l.s; }
+	bool inner(const Pos& p) const { return sign(dir / (p - s)) > 0; }
+	friend bool parallel(const Seg& l0, const Seg& l1) { return zero(l0.dir / l1.dir); }
+	friend bool same_dir(const Seg& l0, const Seg& l1) { return parallel(l0, l1) && l0.dir * l1.dir > 0; }
+	friend Pos intersection_(const Seg& s1, const Seg& s2) {
+		const Pos& p1 = s1.s, & p2 = s1.e;
+		const Pos& q1 = s2.s, & q2 = s2.e;
+		ld a1 = cross(q1, q2, p1);
+		ld a2 = -cross(q1, q2, p2);
+		return (p1 * a2 + p2 * a1) / (a1 + a2);
+	}
+	bool operator < (const Seg& l) const {
+		if (same_dir(*this, l)) return l.inner(s);
+		bool f0 = O < dir;
+		bool f1 = O < l.dir;
+		if (f0 != f1) return f1;
+		return sign(dir / l.dir) > 0;
+	}
+	//bool operator == (const Seg& l) const { return s == l.s && e == l.e; }
 	Pos p(const ld& rt = .5) const { return s + (e - s) * rt; }
 	ld green(const ld& lo = 0, const ld& hi = 1) const {
 		ld d = hi - lo;
@@ -75,348 +134,109 @@ struct Seg {
 	}
 };
 typedef std::vector<Seg> Vseg;
-struct Circle {
-	Pos c;
-	ld r;
-	Circle(Pos c_ = Pos(), ld r_ = 0) : c(c_), r(r_) {}
-	Pos p(const ld& t) const { return c + Pos(r, 0).rot(t); }
-	ld rad(const Pos& p) const { return (p - c).rad(); }
-	ld area(const ld& lo = 0, const ld& hi = 2 * PI) const { return (hi - lo) * r * r * .5; }
-	ld green(const ld& lo, const ld& hi) const {
-		Pos s = Pos(cos(lo), sin(lo)), e = Pos(cos(hi), sin(hi));
-		ld fan = area(lo, hi);
-		Pos m = c + (s + e) * r * (ld).5;
-		ld tz = (cos(lo) - cos(hi)) * m.y * r;
-		return fan + tz - (s / e) * r * r * (ld).5;
+Pos centroid(const Polygon& H) {
+	Pos cen = Pos(0, 0);
+	ld A = 0;
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) {
+		ld a = H[i] / H[(i + 1) % sz];
+		cen += (H[i] + H[(i + 1) % sz]) * a;
+		A += a;
 	}
-	bool operator >= (const Pos& p) const { return r + TOL > (c - p).mag(); }
-	bool operator < (const Pos& p) const { return r < (c - p).mag(); }
+	A *= .5;
+	cen /= 6;
+	if (!zero(A)) cen /= A;
+	return cen;
+}
+struct Pii {
+	int x, y;
+	int i;
+	Pii(int X = 0, int Y = 0) : x(X), y(Y) { i = -1; }
+	bool operator == (const Pii& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pii& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pii& p) const { return x == p.x ? y < p.y : x < p.x; }
+	Pii operator + (const Pii& p) const { return { x + p.x, y + p.y }; }
+	Pii operator - (const Pii& p) const { return { x - p.x, y - p.y }; }
+	Pii operator * (const int& n) const { return { x * n, y * n }; }
+	Pii operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pii& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pii& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
+	ll Euc() const { return (ll)x * x + (ll)y * y;  }
+	friend std::istream& operator >> (std::istream& is, Pii& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pii& p) { os << p.x << " " << p.y; return os; }
 };
-typedef std::vector<Circle> Disks;
-Vld intersections(const Circle& a, const Circle& b) {
-	Pos ca = a.c, cb = b.c;
-	Pos vec = cb - ca;
-	ld ra = a.r, rb = b.r;
-	if (zero(a.r) || zero(b.r)) return {};
-	ld distance = vec.mag();
-	ld rd = vec.rad();
-	if (vec.Euc() > sq(ra + rb) + TOL) return {};
-	if (vec.Euc() < sq(ra - rb) - TOL) return {};
-	ld X = (ra * ra - rb * rb + vec.Euc()) / (2 * distance * ra);
-	if (X < -1) X = -1;
-	if (X > 1) X = 1;
-	ld h = acos(X);
-	Vld ret = {};
-	ret.push_back(norm(rd - h));
-	if (zero(h)) return ret;
-	ret.push_back(norm(rd + h));
+typedef std::vector<Pii> Vpii;
+ll cross(const Pii& d1, const Pii& d2, const Pii& d3) { return (d2 - d1) / (d3 - d2); }
+ll cross(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) { return (d2 - d1) / (d4 - d3); }
+ll dot(const Pii& d1, const Pii& d2, const Pii& d3) { return (d2 - d1) * (d3 - d2); }
+int ccw(const Pii& d1, const Pii& d2, const Pii& d3) { return sign(cross(d1, d2, d3)); }
+int ccw(const Pii& d1, const Pii& d2, const Pii& d3, const Pii& d4) { return sign(cross(d1, d2, d3, d4)); }
+bool on_seg_strong(const Pii& d1, const Pii& d2, const Pii& d3) { ll ret = dot(d1, d3, d2); return !ccw(d1, d2, d3) && ret >= 0; }
+ll area(const Vpii& H) {
+	ll ret = 0;
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) ret += H[i] / H[(i + 1) % sz];
 	return ret;
 }
-Vld circle_line_intersections(const Circle& q, const Pos& s, const Pos& e, const int& f = LINE) {
-	//https://math.stackexchange.com/questions/311921/get-location-of-vector-circle-intersection
-	Pos vec = e - s;
-	Pos OM = s - q.c;
-	ld a = vec.Euc();
-	ld b = vec * OM;
-	ld c = OM.Euc() - q.r * q.r;
-	ld J = b * b - a * c;
-	if (J < -TOL) return {};
-	ld det = sqrt(std::max((ld)0, J));
-	ld lo = (-b - det) / a;
-	ld hi = (-b + det) / a;
-	Vld ret;
-	if (f == LINE) {
-		if (0 < hi && hi < 1) ret.push_back(hi);
-		if (zero(det)) return ret;
-		if (0 < lo && lo < 1) ret.push_back(lo);
-	}
-	else {
-		auto the = [&](ld rt) { return q.rad(s + (e - s) * rt); };
-		if (-TOL < hi && hi < 1 + TOL) ret.push_back(the(hi));
-		if (zero(det)) return ret;
-		if (-TOL < lo && lo < 1 + TOL) ret.push_back(the(lo));
-	}
-	return ret;
+void norm(Vpii& H, const bool& f = 1) {
+	if (f && area(H) < 0) std::reverse(H.begin(), H.end());//ccw
+	if (!f && area(H) > 0) std::reverse(H.begin(), H.end());//cw
+	return;
 }
-ld green(const Circle& c, const ld& lo, const ld& hi) {
-	if (lo < hi) return c.green(lo, hi);
-	return c.green(lo, 2 * PI) + c.green(0, hi);
+bool symmetry(const Vpii& P) {
+	return 1;
 }
-struct Arc {
-	Circle c;
-	ld lo, hi;
-	Arc(Circle c_, ld l_, ld h_) : c(c_), lo(l_), hi(h_) {}
-	bool inside(const Pos& p) const {
-		ld t = norm(c.rad(p));
-		if (lo < hi) return lo <= t && t <= hi;
-		else return lo <= t || t <= hi;
-	}
-};
-typedef std::vector<Arc> Arcs;
-Arcs AR, AL;
-ld area(const Arc& q) { return q.c.area(0, norm(q.hi - q.lo)); }
+ld volume(const Polygon& H, const Pos& cen, const Seg& se) {
+	ld a = area(H);
+	ld d = std::abs(dist(se.s, se.e, cen));
+	return 2 * PI * d * a;
+}
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(5);
-	Circle C; std::cin >> C.c.x >> C.c.y >> C.r;
-	std::cin >> N; Polygon H(N); for (Pos& p : H) std::cin >> p.x >> p.y; norm(H);
-	int r = 0, l = 0;
-	bool meet = 0;
-	bool f0 = 0;
-	ld A = 0;
-	for (int i = 0, j; i < N; i++) {
-		if (ccw(C.c, H[r], H[i]) < 0 || (!ccw(C.c, H[r], H[i]) && dot(C.c, H[r], H[i]) < 0)) r = i;
-		if (ccw(C.c, H[l], H[i]) > 0 || (!ccw(C.c, H[l], H[i]) && dot(C.c, H[l], H[i]) < 0)) l = i;
-		if (dist(H[i], H[(i + 1) % N], C.c, 1) < C.r) meet = 1;
-		j = (i + 1) % N;
-		Vld inxs = circle_line_intersections(C, H[j], H[i], CIRCLE);
-		if (inxs.size() == 2) {
-			ld lo = norm(inxs[0]), hi = norm(inxs[1]);
-			ld t = norm(hi - lo);
-			A += C.area(0, t);
-			A += std::abs(C.r * C.r * .5 * sin(t));
-			std::cout << A << "\n";
-			return;
-		}
-	}
-	if (!meet) { std::cout << C.area() << "\n"; return; }
-	ld lo = norm((H[l] - C.c).rad());
-	for (int j = l, k; 1; j = (j + 1) % N) {
-		k = (j + 1) % N;
-		bool fj = C >= H[j], fk = C >= H[k];
-		if (fj) break;
-		if (fk) {
-			Vld inx = circle_line_intersections(C, H[k], H[j], CIRCLE);
-			assert(inx.size() == 1);
-			lo = inx[0];
-			l = j;
-			break;
-		}
-	}
-	ld hi = norm((H[r] - C.c).rad());
-	for (int j = r, i; 1; j = (j - 1 + N) % N) {
-		i = (j - 1 + N) % N;
-		bool fj = C >= H[j], fi = C >= H[i];
-		if (fj) break;
-		if (fi) {
-			Vld inx = circle_line_intersections(C, H[i], H[j], CIRCLE);
-			assert(inx.size() == 1);
-			hi = inx[0];
-			r = j;
-			break;
-		}
-	}
-	A = 0;
-	for (int i = l, j; i != r; i = (i + 1) % N) {
-		j = (i + 1) % N;
-		bool fi = C >= H[i], fj = C >= H[j];
-		if (fi && fj) {
-			A += cross(C.c, H[j], H[i]) * .5;
-			continue;
-		}
-		Vld inx = circle_line_intersections(C, H[j], H[i]);
-		assert(inx.size() == 1);
-		ld x = inx[0];
-		if (fi) A += cross(C.c, Seg(H[j], H[i]).p(x), H[i]) * .5;
-		if (fj) A += cross(C.c, H[j], Seg(H[j], H[i]).p(x)) * .5;
-	}
-	A += C.area(0, norm(hi - lo));
-	int el = l, er = r;
-	Circle cl, cr;
-	ld aa = 0;
-	ld L = C.r;
-	for (int j = l, i, k; 1; j = (j - 1 + N) % N) {
-		i = (j - 1 + N) % N;
-		k = (j + 1) % N;
-		lo = norm((H[i] - H[j]).rad());
-		hi = norm((H[j] - H[k]).rad());
-		if (j == l) hi = norm((H[j] - C.c).rad());
-		ld d = (H[j] - H[k]).mag();
-		if (j == l) d = (H[j] - C.c).mag();
-		if (L <= d) break;
-		L -= d;
-		Circle c = Circle(H[j], L);
-		AL.push_back(Arc(c, lo, hi));
-		if (c < H[i]) { el = j; break; }
-	}
-	ld R = C.r;
-	for (int j = r, i, k; 1; j = (j + 1) % N) {
-		i = (j - 1 + N) % N;
-		k = (j + 1) % N;
-		lo = norm((H[j] - H[i]).rad());
-		hi = norm((H[k] - H[j]).rad());
-		if (j == r) lo = norm((H[j] - C.c).rad());
-		ld d = (H[j] - H[i]).mag();
-		if (j == r) d = (H[j] - C.c).mag();
-		if (R <= d) break;
-		R -= d;
-		Circle c = Circle(H[j], R);
-		AR.push_back(Arc(c, lo, hi));
-		if (c < H[k]) { er = j; break; }
-	}
-	if (AL.size() > 0 && AR.size() > 0) {
-		Vld inxs = intersections(AR.back().c, AL.back().c);
-		ld x = 0;
-		if ((er + 1) % N == el && inxs.size() == 2) {
-			std::deque<Pos> dq;
-			Pos m;
-			while (1) {
-				//std::cout << "FUCK::\n";
-				int szl = AL.size();
-				int szr = AR.size();
-				if (szl < 1 || szr < 1) {
-					std::cout << "something wrong::\n";
-					return;
-				}
-				inxs = intersections(AR.back().c, AL.back().c);
-				//std::cout << AR.back().c.c.x << " ";
-				//std::cout << AR.back().c.c.y << " ";
-				//std::cout << AR.back().c.r << "\n";
-				//std::cout << AL.back().c.c.x << " ";
-				//std::cout << AL.back().c.c.y << " ";
-				//std::cout << AL.back().c.r << "\n";
-				//std::cout << inxs.size() << "\n";
-				x = inxs[0];
-				m = AR.back().c.p(x);
-				bool fl = AL.back().inside(m);
-				bool fr = AR.back().inside(m);
-				if (fl && fr) {
-					dq.push_back(AL.back().c.c); dq.push_front(AR.back().c.c);
-					break;
-				}
-				if (!fl && !fr) {
-					bool ffr = 0, ffl = 0;
-					if (szl > 1) {
-						Arc a = AL[szl - 2];
-						inxs = intersections(AR.back().c, a.c);
-						x = inxs[0];
-						m = AR.back().c.p(x);
-						if (a.inside(m) && AR.back().inside(m)) ffl = 1;
+	std::cout.precision(15);
+	std::cin >> N;
+	Vpii P(N); for (Pii& p : P) std::cin >> p; norm(P);
+	ld vol = 0;
+	if (N & 1) {
+		int cnt = 0;
+		Seg s1, s2;
+		for (int i = 0, j = 1; i < N; i++) {
+			while (ccw(P[i], P[(i + 1) % N], P[j], P[(j + 1) % N]) > 0) j = (j + 1) % N;
+			if ((P[i] - P[j]).Euc() == (P[(i + 1) % N] - P[j]).Euc()) {
+				if (symmetry(P)) {
+					cnt++;
+					if (cnt == 1) {
+						s1;
+						Polygon H;
+						Pos cen = centroid(H);
+						vol = volume(H, cen, s1);
 					}
-					if (szr > 1) {
-						Arc a = AR[szr - 2];
-						inxs = intersections(a.c, AL.back().c);
-						x = inxs[0];
-						m = a.c.p(x);
-						if (a.inside(m) && AL.back().inside(m)) ffr = 1;
+					if (cnt == 2) {
+						s2;
+						break;
 					}
-					if (!ffl) { dq.push_front(AR.back().c.c); AR.pop_back(); }
-					if (!ffr) { dq.push_back(AL.back().c.c); AL.pop_back(); }
-					continue;
 				}
-				if (!fl) { dq.push_back(AL.back().c.c); AL.pop_back(); continue; }
-				if (!fr) { dq.push_front(AR.back().c.c); AR.pop_back(); continue; }
 			}
-			ld t = 0;
-			Circle cl = AL.back().c;
-			Circle cr = AR.back().c;
-			inxs = intersections(cr, cl);
-			x = inxs[0];
-			m = cr.p(x);
-			A += cross(cr.c, m, cl.c) * .5;
-			ld lo = cl.rad(m);
-			t = norm(AL.back().hi - lo);
-			A += cl.area(0, t);
-			ld hi = cr.rad(m);
-			t = norm(hi - AR.back().lo);
-			A += cr.area(0, t);
-			int sz = dq.size();
-			for (int i = 0; i < sz; i++) A -= dq[i] / dq[(i + 1) % sz] * .5;
-			AL.pop_back(); AR.pop_back();
+			if (cnt > 1) break;
+		}
+		if (cnt == 0) std::cout << 0 << "\n";
+		else if (cnt == 1) std::cout << vol << "\n";
+		else {
+			Pos m = intersection(s1.s, s1.e, s2.s, s2.e);
+			for (int i = 0; i < N; i++) {
+			}
+			std::cout << vol << "\n";
 		}
 	}
-	for (const Arc& q : AL) A += area(q);
-	for (const Arc& q : AR) A += area(q);
-	std::cout << A << "\n";
+
+
 	return;
 }
-int main() { solve(); return 0; }//boj14873
+int main() { solve(); return 0; }//boj25646
 //boj30123 27712 3607 10239
 
 /*
-
-150 160 100
-4
-10 60
-150 120
-300 50
-150 10
-
-20 90 328
-10
-40 90
-80 210
-130 230
-190 220
-210 180
-210 100
-180 50
-130 30
-80 20
-60 40
-294892.948786434659269
-
-10 60 180
-5
-60 -10
-30 0
-20 60
-30 90
-60 130
-89371.544433201986458
-
-853 271 1110
-49
-599 275
-593 238
-587 213
-583 201
-563 155
-535 114
-527 104
-491 69
-471 54
-450 40
-416 23
-393 15
-331 2
-319 1
-294 0
-269 2
-195 19
-172 29
-109 69
-90 86
-65 114
-43 145
-31 167
-1 275
-1 325
-7 362
-50 466
-65 486
-129 546
-150 560
-172 571
-184 577
-207 585
-219 589
-244 595
-269 598
-281 599
-331 598
-369 592
-393 585
-405 581
-428 571
-450 560
-491 531
-519 505
-535 486
-543 476
-579 410
-587 387
 
 */

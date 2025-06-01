ad563f4cd31685a58ebd182a807658f3a176752a
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-06-01 18:11:48 +0900

    25

--- BOJ/main/boj_bronze.cpp
+++ BOJ/main/boj_bronze.cpp
@@ -6,20 +6,23 @@
 #include <cassert>
 #include <vector>
 typedef long long ll;
-//typedef long double ld;
-typedef double ld;
+typedef long double ld;
+//typedef double ld;
 typedef std::pair<int, int> pi;
 typedef std::vector<int> Vint;
 typedef std::vector<ld> Vld;
 
 int N;
-ll T, S, M, Ds, Ys, Dm, Ym;
+ll X, Y, Z;
 int I[10];
+ld T;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> I[0] >> I[1] >> I[2];
-	std::sort(I, I + 3);
-	std::cout << I[1] << "\n";
+	std::cout << std::fixed;
+	std::cout.precision(9);
+	std::cin >> X >> Y >> Z;
+
+
 	return 0;
 }

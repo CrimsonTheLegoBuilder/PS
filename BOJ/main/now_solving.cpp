f9c5ad3f54f0f91f4c56364544f3799dce98269a
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-06-13 20:03:00 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <algorithm>
 #include <cmath>
+#include <string>
 #include <cstring>
 #include <cassert>
 #include <vector>
@@ -181,13 +182,15 @@ Range range(const Seg& s1, const Seg& s2, const Pos& o) {
 	if (i2) f2 = intersection(s1, Seg(p0, q2));
 	return Range(f1, f2);
 }
+#define BOJ
+#ifdef BOJ
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(13);
-	freopen("../../tests/aed/input/input0.txt", "r", stdin);
-	freopen("../../tests/aed/input/ret.txt", "w", stdout);
+	//freopen("../../tests/aed/input/input0.txt", "r", stdin);
+	//freopen("../../tests/aed/input/ret.txt", "w", stdout);
 	Pos u;
 	std::cin >> N; Polygon H(N); std::cin >> u;
 	for (Pos& p : H) std::cin >> p; norm(H);
@@ -236,6 +239,80 @@ void solve() {
 	for (int i : I) std::cout << i << " ";
 	return;
 }
+#else
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(13);
+	int Q = 130;
+	for (int q = 0; q <= Q; q++) {
+		std::string Din = "../../tests/aed/input/input";
+		std::string Dout = "../../tests/aed/output/output";
+		std::string Qin = Din + std::to_string(q) + ".txt";
+		std::string Qout = Dout + std::to_string(q) + ".txt";
+		freopen(Qin.c_str(), "r", stdin);
+		//freopen("../../tests/aed/input/ret.txt", "w", stdout);
+		Pos u;
+		std::cin >> N; Polygon H(N); std::cin >> u;
+		for (Pos& p : H) std::cin >> p; norm(H);
+		Vint I, J;
+		for (int i = 0, i1; i < N; i++) {
+			i1 = (i + 1) % N;
+			Pos p0 = H[i], p1 = H[i1];
+			bool vis;
+			if (!ccw(u, p0, p1)) {
+				if (dot(p0, p1, u) < 0) std::swap(p0, p1);
+				vis = 1;
+				for (int j = 0, j1; j < N; j++) {
+					if (j == i) continue;
+					j1 = (j + 1) % N;
+					Pos q0 = H[j], q1 = H[j1];
+					if (ccw(u, q0, q1) < 0) std::swap(q0, q1);
+					if (intersect(q0, q1, p1, u)) { vis = 0; break; }
+				}
+			}
+			else {
+				if (ccw(u, p0, p1) < 0) std::swap(p0, p1);
+				Seg s1 = Seg(p0, p1);
+				Vrange V;
+				for (int j = 0, j1; j < N; j++) {
+					if (j == i) continue;
+					j1 = (j + 1) % N;
+					Pos q0 = H[j], q1 = H[j1];
+					if (ccw(u, q0, q1) < 0) std::swap(q0, q1);
+					Seg s2 = Seg(q0, q1);
+					Range r = range(s1, s2, u);
+					if (r.s.den != -1) V.push_back(r);
+				}
+				vis = 0;
+				std::sort(V.begin(), V.end());
+				V.push_back(Range(Frac(1), Frac(1)));
+				int sz = V.size();
+				Frac hi = Frac(0);
+				for (const Range& r : V) {
+					if (hi < r.s) { vis = 1; break; }
+					else hi = std::max(hi, r.e);
+				}
+			}
+			if (vis) I.push_back(i + 1);
+		}
+		//std::cout << I.size() << "\n";
+		//for (int i : I) std::cout << i << " ";
+		freopen(Qout.c_str(), "r", stdin);
+		std::cin >> M;
+		J.resize(M); for (int& j : J) std::cin >> j;
+		if (I.size() != M) { std::cout << "fuck::\n"; continue; }
+		bool f = 1;
+		for (int j = 0; j < M; j++) {
+			if (I[j] != J[j]) { f = 0; std::cout << "fuck::\n"; break; }
+		}
+		if (f) std::cout << "good::\n";
+		else std::cout << "fuck::\n";
+	}
+	return;
+}
+#endif
 int main() { solve(); return 0; }//boj29931
 //boj 27712 10239 22635 29691 31392
 

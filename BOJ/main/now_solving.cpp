2285b4a16e74f53459cf8e3e0286a9507e30b801
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-07-22 18:37:58 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -8,71 +8,13 @@
 typedef long long ll;
 //typedef long double ld;
 typedef double ld;
-typedef std::pair<int, int> pi;
-typedef std::vector<int> Vint;
-typedef std::vector<ld> Vld;
-const ll INF = 1e17;
-const int LEN = 1e5 + 1;
-const ld TOL = 1e-7;
-const ll MOD = 1e9 + 7;
-const ld PI = acos(-1);
-inline int sign(const int& x) { return x < 0 ? -1 : !!x; }
-inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 
 int N, M;
-struct Pos {
-	int x, y;
-	Pos(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
-	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
-	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
-	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
-	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const int& n) const { return { x * n, y * n }; }
-	Pos operator / (const int& n) const { return { x / n, y / n }; }
-	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
-	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
-	ll Euc() const { return (ll)x * x + (ll)y * y; }
-	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
-	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
-} H[50000];
-const Pos INVAL = Pos(-1, -1);
-typedef std::vector<Pos> Polygon;
-ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !cross(d1, d2, d3) && dot(d1, d3, d2) >= 0; }
-int inner_check_bi_search(const Pos H[], const int& sz, const Pos& p) {//convex
-	if (cross(H[0], H[1], p) < 0 || cross(H[0], H[sz - 1], p) > 0) return 0;
-	if (on_seg_strong(H[0], H[1], p) || on_seg_strong(H[0], H[sz - 1], p)) return 1;
-	int s = 0, e = sz - 1, m;
-	while (s + 1 < e) {
-		m = s + e >> 1;
-		if (cross(H[0], H[m], p) >= 0) s = m;
-		else e = m;
-	}
-	return on_seg_strong(H[s], H[e], p) || cross(H[s], H[e], p) > 0;
-}
-void solve() {
+int main() { 
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
-	std::cin >> N; for (int i = 0; i < N; i++) std::cin >> H[i];
-	std::swap(H[0], *std::min_element(H, H + N));
-	std::sort(H + 1, H + N, [&](const Pos& p, const Pos& q) {
-		ll ret = cross(H[0], p, q);
-		return ret == 0 ? (H[0] - p).Euc() < (H[0] - q).Euc() : ret > 0;
-		});
-	int S = -1;
-	for (int i = 0; i < N; i++) {
-		while (S >= 1 && cross(H[S - 1], H[S], H[i]) <= 0) S--;
-		H[++S] = H[i];
-	}
-	N = S + 1;
-	std::cin >> M;
-	Pos p;
-	int c = 0;
-	while (M--) {
-		std::cin >> p;
-		c += inner_check_bi_search(H, N, p);
-	}
-	std::cout << c << "\n";
+	for (int _ = 0; _ < 4; _++) std::cin >> N, M += N;
+	if (M <= 1500) std::cout << "Yes\n";
+	else std::cout << "No\n";
+	return 0;
 }
-int main() { solve(); return 0; }

524ce4c5e9b8148e54d3945d8f3a70a2cea0d30b
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-07-27 22:52:44 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -9,21 +9,23 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 
-int Q, N, M, K, P, C, L, T, a, d, g, b, s, t;
+int Q, N, M, Y, K, P, C, L, T; ll a, d, c, g, b, s, t, x, y;
+int q1, q2, q3, q4, q0;
 char B[1001];
 int main() { 
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> N >> M >> K;
-	if (N + M == K) { std::cout << N << "+" << M << "=" << K << "\n"; return 0; }
-	if (N - M == K) { std::cout << N << "-" << M << "=" << K << "\n"; return 0; }
-	if (N * M == K) { std::cout << N << "*" << M << "=" << K << "\n"; return 0; }
-	if (N / M == K && !(N % M)) { std::cout << N << "/" << M << "=" << K << "\n"; return 0; }
-	if (N == M + K) { std::cout << N << "=" << M << "+" << K << "\n"; return 0; }
-	if (N == M - K) { std::cout << N << "=" << M << "-" << K << "\n"; return 0; }
-	if (N == M * K) { std::cout << N << "=" << M << "*" << K << "\n"; return 0; }
-	//if (N == M / K && !(M % K)) { std::cout << N << "=" << M << "/" << K << "\n"; return 0; }
+	std::cin >> T;
+	while (T--) {
+		std::vector<int> I(7);
+		for (int& i : I) std::cin >> i;
+		std::sort(I.begin(), I.end());
+		N = 0; M = 0;
+		for (const int& i : I) if (!(i & 1)) { M = i; break; }
+		for (const int& i : I) if (!(i & 1)) N += i;
+		std::cout << N << " " << M << "\n";
+	}
 	return 0;
 }

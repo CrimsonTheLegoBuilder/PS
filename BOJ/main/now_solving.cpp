8bbbcc3dd1202e7893443eb5c7033fd471399427
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-06 23:48:17 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -9,7 +9,8 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 
-int Q, N, M, Y, K, P, C, L, T; ll a, d, c, g, b, s, t, x, y;
+int Q, N, M, Y, K, P, C, L, T;
+ll a, d, c, g, b, s, t, x, y, w1, h1, w2, h2;
 int q1, q2, q3, q4, q0;
 char B[1001];
 int main() { 
@@ -17,15 +18,10 @@ int main() {
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> T;
-	while (T--) {
-		std::vector<int> I(7);
-		for (int& i : I) std::cin >> i;
-		std::sort(I.begin(), I.end());
-		N = 0; M = 0;
-		for (const int& i : I) if (!(i & 1)) { M = i; break; }
-		for (const int& i : I) if (!(i & 1)) N += i;
-		std::cout << N << " " << M << "\n";
-	}
+	std::cin >> w1 >> h1 >> w2 >> h2;
+	T = 0;
+	T += h1 + h2;
+	T += std::max(w1, w2);
+	std::cout << (T << 1) + 4 << "\n";
 	return 0;
 }

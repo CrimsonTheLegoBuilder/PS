ff70181fe708f26fc0eaf0f273c5ea013f7e0ebd
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-08 20:28:10 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -10,7 +10,7 @@ typedef long long ll;
 typedef double ld;
 
 int Q, N, M, Y, K, P, C, L, T, n, m, k;
-ll a, d, c, g, b, s, t, x, y, w, l, h;
+ll a, d, c, g, b, s, t, x, y, w, l, h, p, q;
 int q1, q2, q3, q4, q0;
 char B[1001];
 int main() { 
@@ -18,22 +18,9 @@ int main() {
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> N >> M >> K;
-	T = std::min(M, N / 2);
-	//std::cout << "T:: " << T << "\n";
-	n = N - T * 2;
-	m = M - T;
-	//std::cout << "n:: " << n << "\n";
-	//std::cout << "m:: " << m << "\n";
-	K -= n;
-	K -= m;
-	K = std::max(K, 0);
-	k = K % 3;
-	K /= 3;
-	//std::cout << "k:: " << k << "\n";
-	//std::cout << "K:: " << K << "\n";
-	K += k ? 1 : 0;
-	T -= K;
-	std::cout << T << "\n";
+	std::cin >> a >> b >> c;
+	n = b - a;
+	m = c - b;
+	std::cout << std::max(std::max(n, m) - 1, 0) << "\n";
 	return 0;
 }

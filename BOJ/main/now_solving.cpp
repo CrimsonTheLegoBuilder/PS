44c780bd2934efe6e9cdc6e083ee6f1afe70c337
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-13 00:02:53 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -9,18 +9,25 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 
-int Q, N, M, K;
-int q1, q2, q3, q4, q0, n[3];
+int Q, N, M, K, a, b, T;
+int q1, q2, q3, q4, q0, n[3], g, p ,t;
 char B[1001];
+std::string S;
 int main() { 
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> n[0] >> n[1] >> n[2];
-	std::sort(n, n + 3);
-	if (n[0] + n[1] == n[2]) std::cout << "1\n";
-	else if (n[0] * n[1] == n[2]) std::cout << "2\n";
-	else std::cout << "3\n";
+	std::cin >> N;
+	while (N--) {
+		std::cin >> M >> K >> T;
+		std::cout << "Data set: " << M << " " << K << " " << T << "\n";
+		while (T--) {
+			if (M < K) std::swap(M, K);
+			M /= 2;
+		}
+		if (M < K) std::swap(M, K);
+		std::cout << M << " " << K << "\n\n";
+	}
 	return 0;
 }

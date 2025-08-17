eb2dba06ac8662fb89d8898d5f3cfd2483bb217e
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-17 19:30:41 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -9,18 +9,18 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 
-int Q, N, M, K, a, b, c, T, W, H, s, o;
+int Q, N, M, K, a, b, c, T, W, H, L, s, o;
 int q1, q2, q3, q4, q0, n[3], g, p ,t, A, B, C, D, P, I;
-int ret[2001];
+int F[2001];
 std::string S;
 int main() { 
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	std::cin >> A >> B >> C >> D >> P;
-	a = A * P;
-	b = B + (std::max(0, P - C)) * D;
-	std::cout << std::min(a, b) << "\n";
+	std::cin >> W >> H >> N >> A >> B;
+	if (W < A || H < B) { std::cout << "-1\n"; return 0; }
+	T = (W / A) * (H / B);
+	std::cout << (N / T) + (!(N % T) ? 0 : 1) << "\n";
 	return 0;
 }

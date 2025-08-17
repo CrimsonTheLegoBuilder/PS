8446ea3791ce86e319d6426df511b74178c01308
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-18 00:26:32 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -10,23 +10,20 @@ typedef long long ll;
 typedef double ld;
 
 int Q, N, M, K, a, b, c, T, W, H, L, s, o;
-int q1, q2, q3, q4, q0, n[3], g,t, A, B, C, D, E, P, I;
-int F[2001];
-ld p, p0, X, Y;
+//int q1, q2, q3, q4, q0, n[3], g,t, A, B, C, D, E, P, I;
+//int F[2001];
+ld p, p0, X, Y, D, A, B, F;
 std::string S;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(2);
+	std::cout.precision(6);
 	std::cin >> T;
 	while (T--) {
-		std::cin >> N;
-		K = 0;
-		for (int _ = 0; _ < N; _++) {
-			std::cin >> M; K += M;
-		}
-		std::cout << (K % N ? "NO\n" : "YES\n");
+		std::cin >> N >> D >> A >> B >> F;
+		std::cout << N << " ";
+		std::cout << (D / (A + B)) * F << "\n";
 	}
 	return 0;
 }

595cd9199eb178240a7c5a4ccecfc79bd6e6035c
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-19 23:07:58 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -12,38 +12,26 @@ const ld PI = acos(-1);
 
 int Q, N, M, K, a, b, c, T, W, H, L, s, o;
 int q1, q2, q3, q4, q0, A, B, C, D, E, P, S, I;
-//int F[2001];
+int F[8];
 //ld p, p0, X, Y, D, A, B, F;
 //std::string S;
-void cal_mile(ld d, ld f, ld t, ld& m, ld& mph) {
-	d /= 12.;
-	d /= 5280.;
-	d *= f;
-	d *= PI;
-	m = d;
-	mph = d / t * 3600;
-	return;
-}
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(2);
-	N = 1;
-	//ld d = 1;
-	//d /= 12.;
-	//d /= 5280.;
-	//std::cout << d << "\n";
-	while (1) {
-		ld d, t;
-		int f;
-		std::cin >> d >> f >> t;
-		if (!f) break;
-		std::cout << "Trip #" << N << ": ";
-		ld m, mph;
-		cal_mile(d, f, t, m, mph);
-		std::cout << m << " " << mph << "\n";
-		N++;
+	std::cin >> A >> B >> C;
+	F[0] = A;
+	F[1] = B;
+	F[2] = C;
+	F[3] = A * B;
+	F[4] = B * C;
+	F[5] = C * A;
+	F[6] = A * B * C;
+	std::sort(F, F + 7);
+	for (int i = 6; i > -1; i--) {
+		if (F[i] & 1) { std::cout << F[i] << "\n"; return 0; }
 	}
+	std::cout << F[6] << "\n";
 	return 0;
 }

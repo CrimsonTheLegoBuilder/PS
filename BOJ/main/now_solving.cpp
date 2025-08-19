1b913525c83604269f836a364a2a4747c95f5547
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-19 22:33:10 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -8,22 +8,42 @@
 typedef long long ll;
 //typedef long double ld;
 typedef double ld;
+const ld PI = acos(-1);
 
 int Q, N, M, K, a, b, c, T, W, H, L, s, o;
-int q1, q2, q3, q4, q0, n[3], g,t, A, B, C, D, E, P, S, I;
+int q1, q2, q3, q4, q0, A, B, C, D, E, P, S, I;
 //int F[2001];
 //ld p, p0, X, Y, D, A, B, F;
 //std::string S;
+void cal_mile(ld d, ld f, ld t, ld& m, ld& mph) {
+	d /= 12.;
+	d /= 5280.;
+	d *= f;
+	d *= PI;
+	m = d;
+	mph = d / t * 3600;
+	return;
+}
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(6);
-	std::cin >> T;
-	while (T--) {
-		std::cin >> N >> M;
-		if (N < 12) { std::cout << "-1\n"; continue; }
-		std::cout << M * 11 + 4 << "\n";
+	std::cout.precision(2);
+	N = 1;
+	//ld d = 1;
+	//d /= 12.;
+	//d /= 5280.;
+	//std::cout << d << "\n";
+	while (1) {
+		ld d, t;
+		int f;
+		std::cin >> d >> f >> t;
+		if (!f) break;
+		std::cout << "Trip #" << N << ": ";
+		ld m, mph;
+		cal_mile(d, f, t, m, mph);
+		std::cout << m << " " << mph << "\n";
+		N++;
 	}
 	return 0;
 }

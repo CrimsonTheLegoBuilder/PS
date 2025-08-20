bd5001024f5fef63d522a025827d018a21c52acf
crimson <redcube231@gmail.com>
2025-08-21 08:13:39 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <cmath>
 #include <cstring>
+#include <string>
 #include <cassert>
 #include <vector>
 typedef long long ll;
@@ -16,16 +17,23 @@ int F[8];
 ll TT[305];
 ll TTT[305];
 //ld p, p0, X, Y, D, A, B, F;
-//std::string S;
+std::string S[101];
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(14);
-	while (std::cin >> A >> B >> C) {
-		N = B - A;
-		M = C - B;
-		std::cout << std::max(N, M) - 1 << "\n";
+	std::cin >> N;
+	while (N--) {
+		std::cin >> M;
+		std::cout << "Pairs for ";
+		std::cout << M << ": ";
+		K = M - 1;
+		for (int i = 1; i <= K / 2; i++) {
+			std::cout << i << " " << M - i;
+			if (i <= K / 2 - 1) std::cout << ", ";
+			else std::cout << "\n";
+		}
 	}
 	return 0;
 }

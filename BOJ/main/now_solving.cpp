ebe0de622563a73cc70e654bdfda4ae326074818
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-21 00:01:39 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -13,21 +13,19 @@ const ld PI = acos(-1);
 int Q, N, M, K, a, b, c, T, W, H, L, s, o;
 int q1, q2, q3, q4, q0, A, B, C, D, E, P, I;
 int F[8];
-char S[15];
+ll TT[305];
+ll TTT[305];
 //ld p, p0, X, Y, D, A, B, F;
 //std::string S;
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(4);
-	std::cin >> N;
-	ld t = 0;
-	while (N--) {
-		ld q, y;
-		std::cin >> q >> y;
-		t += q * y;
+	std::cout.precision(14);
+	while (std::cin >> A >> B >> C) {
+		N = B - A;
+		M = C - B;
+		std::cout << std::max(N, M) - 1 << "\n";
 	}
-	std::cout << t << "\n";
 	return 0;
 }

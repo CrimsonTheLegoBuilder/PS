df294f9ae8d5e74c7f38f792d5897675af11d3d3
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-21 22:37:25 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -10,30 +10,34 @@ typedef long long ll;
 //typedef long double ld;
 typedef double ld;
 const ld PI = acos(-1);
+const ll MOD = 998244353;
 
-int Q, N, M, K, a, b, c, T, W, H, L, s, o;
+int Q, N, M, K, a, b, c, d, T, W, H, L, R, s, o;
 int q1, q2, q3, q4, q0, A, B, C, D, E, P, I;
-int F[8];
+int F[200005];
 ll TT[305];
 ll TTT[305];
 //ld p, p0, X, Y, D, A, B, F;
 std::string S[101];
+ll pow_mod(ll N, int K) {
+	if (!K) return 1;
+	ll h = pow_mod(N, K / 2);
+	ll ret = (h * h) % MOD;
+	if (K & 1) ret = (ret * N) % MOD;
+	return ret % MOD;
+}
 int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(14);
 	std::cin >> N;
-	while (N--) {
-		std::cin >> M;
-		std::cout << "Pairs for ";
-		std::cout << M << ": ";
-		K = M - 1;
-		for (int i = 1; i <= K / 2; i++) {
-			std::cout << i << " " << M - i;
-			if (i <= K / 2 - 1) std::cout << ", ";
-			else std::cout << "\n";
-		}
+	T = 0;
+	for (int i = 0; i < N; i++) {
+		std::cin >> K;
+		K = std::max(K - (N - i), 0);
+		T = std::max(T, K);
 	}
+	std::cout << T << "\n";
 	return 0;
 }

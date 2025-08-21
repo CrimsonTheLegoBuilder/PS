0c14383995cdf9ba17a717c338f330491db27d84
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-08-22 00:41:13 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -13,10 +13,9 @@ const ld PI = acos(-1);
 const ll MOD = 998244353;
 
 int Q, N, M, K, a, b, c, d, T, W, H, L, R, s, o;
-int q1, q2, q3, q4, q0, A, B, C, D, E, P, I;
-int F[200005];
-ll TT[305];
-ll TTT[305];
+int q1, q2, q3, q4, q0, A[100], B[100], C, D, E, P, I;
+int F[5];
+int X;
 //ld p, p0, X, Y, D, A, B, F;
 std::string S[101];
 ll pow_mod(ll N, int K) {
@@ -30,13 +29,12 @@ int main() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
-	std::cout.precision(14);
+	std::cout.precision(2);
 	std::cin >> N;
-	T = 0;
-	for (int i = 0; i < N; i++) {
-		std::cin >> K;
-		K = std::max(K - (N - i), 0);
-		T = std::max(T, K);
+	if (N > 198) { std::cout << "0\n"; return 0; }
+	for (int i = 0; i <= std::min(99, N); i++) {
+		K = N - i;
+		if (K < 100) T++;
 	}
 	std::cout << T << "\n";
 	return 0;

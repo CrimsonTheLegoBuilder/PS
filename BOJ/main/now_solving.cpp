5b1effe845af5d16cb2833acf1595e9a41fdfdda
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-10-16 10:54:07 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -5,134 +5,81 @@
 #include <cstring>
 #include <cassert>
 #include <vector>
-#include <queue>
-#include <deque>
-#include <random>
-#include <array>
-#include <tuple>
-#include <complex>
 typedef long long ll;
-//typedef long double ld;
 typedef double ld;
-typedef std::pair<int, int> pi;
-typedef std::vector<int> Vint;
-typedef std::vector<ld> Vld;
-const ld INF = 1e17;
-const ld TOL = 1e-7;
-const ld PI = acos(-1);
-const int LEN = 1e3;
-inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
-inline bool zero(const ld& x) { return !sign(x); }
-inline bool eq(const ld& x, const ld& y) { return zero(x - y); }
-inline ld sq(const ld& x) { return x * x; }
-inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
-inline ld fit(const ld& x, const ld& lo, const ld& hi) { return std::min(hi, std::max(lo, x)); }
-ld flip(ld lat) {
-	if (zero(lat - PI * .5) || zero(lat + PI * .5)) return 0;
-	if (zero(lat)) return PI * .5;
-	if (lat > 0) return PI * .5 - lat;
-	if (lat < 0) return -(PI * .5) - lat;
-	return INF;
-}
+const ll INF = 1e17;
+const int LEN = 1e5 + 1;
+inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 
-int N, M, K, T, Q;
+int N, T;
 struct Pos {
-	ld x, y;
-	Pos(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) {}
-	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
-	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
-	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
-	bool operator <= (const Pos& p) const { return *this < p || *this == p; }
+	int x, y, i;
+	Pos(int x_ = 0, int y_ = 0, int i_ = -1) : x(x_), y(y_), i(i_) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
-	Pos operator * (const ld& n) const { return { x * n, y * n }; }
-	Pos operator / (const ld& n) const { return { x / n, y / n }; }
-	ld operator * (const Pos& p) const { return x * p.x + y * p.y; }
-	ld operator / (const Pos& p) const { return x * p.y - y * p.x; }
-	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
-	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
-	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
-	Pos& operator *= (const ld& n) { x *= n; y *= n; return *this; }
-	Pos& operator /= (const ld& n) { x /= n; y /= n; return *this; }
-	Pos operator - () const { return { -x, -y }; }
-	Pos operator ~ () const { return { -y, x }; }
-	Pos operator ! () const { return { y, x }; }
-	ld xy() const { return x * y; }
-	Pos rot(const ld& t) const { return { x * cos(t) - y * sin(t), x * sin(t) + y * cos(t) }; }
-	ld Euc() const { return x * x + y * y; }
-	ld mag() const { return sqrt(Euc()); }
-	Pos unit() const { return *this / mag(); }
-	ld rad() const { return atan2(y, x); }
-	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
-	int quad() const { return sign(y) == 1 || (sign(y) == 0 && sign(x) >= 0); }
-	friend bool cmpq(const Pos& a, const Pos& b) { return (a.quad() != b.quad()) ? a.quad() < b.quad() : a / b > 0; }
-	bool close(const Pos& p) const { return zero((*this - p).Euc()); }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
-}; const Pos O = { 0, 0 };
+}; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
-ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
-int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return sign(cross(d1, d2, d3, d4)); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
-ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
-bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && sign(dot(d1, d3, d2)) >= 0; }
-bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && sign(dot(d1, d3, d2)) > 0; }
-ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3) / (d2 - d1).mag(); }
-ld dist(const Pos& d1, const Pos& d2, const Pos& t, bool f = 0) {
-	if (!f) return cross(d1, d2, t) / (d1 - d2).mag();
-	if (sign(projection(d1, d2, d2, t)) <= 0 &&
-		sign(projection(d2, d1, d1, t)) <= 0)
-		return std::abs(cross(d1, d2, t)) / (d1 - d2).mag();
-	return std::min((d1 - t).mag(), (d2 - t).mag());
+ll area(Polygon& H) {
+	ll a = 0; int sz = H.size();
+	for (int i = 0; i < sz; i++) a += H[i] / H[(i + 1) % sz];
+	return a;
 }
-bool collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return !ccw(d1, d2, d3) && !ccw(d1, d2, d4); }
-bool between(const Pos& d0, const Pos& d1, const Pos& q) { return sign(dot(d0, d1, q)) < 0 && sign(dot(d1, d0, q)) < 0; }
-Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) { ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2); return (p1 * a2 + p2 * a1) / (a1 + a2); }
-ld area(const Polygon& H) {
-	ld A = 0;
-	int sz = H.size();
-	for (int i = 0; i < sz; i++) A += H[i] / H[(i + 1) % sz];
-	return A * .5;
-}
-ld vol(const ld& hi, const Polygon& P) {
-	Pos t = P[0];
-	int sz = P.size();
-	for (int i = 1; i < sz; i++) {
-
+void norm(Polygon& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
+Polygon graham_scan(Polygon& C) {
+	Polygon H;
+	if (C.size() < 3) {
+		std::sort(C.begin(), C.end());
+		return C;
 	}
+	std::swap(C[0], *min_element(C.begin(), C.end()));
+	std::sort(C.begin() + 1, C.end(), [&](const Pos& p, const Pos& q) -> bool {
+		int ret = ccw(C[0], p, q);
+		if (!ret) return (C[0] - p).Euc() < (C[0] - q).Euc();
+		return ret > 0;
+		}
+	);
+	C.erase(unique(C.begin(), C.end()), C.end());
+	int sz = C.size();
+	for (int i = 0; i < sz; i++) {
+		while (H.size() >= 2 && ccw(H[H.size() - 2], H.back(), C[i]) <= 0)
+			H.pop_back();
+		H.push_back(C[i]);
+	}
+	return H;
 }
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(9);
-	std::cin >> N;
-	Polygon P(N);
-	ld s = 0, e = INF;
-	ld w = 0;
-	for (Pos& p : P) {
-		ld x, y, z;
-		std::cin >> x >> y >> z;
-		ld x_ = Pos(x, y).mag();
-		p = Pos(x_, z);
-		s = std::max(s, z);
-		w = std::max(w, x_);
-	}
-	P.push_back(Pos(0, s));
-	P.push_back(Pos(w, 0));
-	Polygon S;
-	std::sort(P.rbegin(), P.rend());
-	for (int i = 0; i < P.size(); i++) {
-		while (S.size() > 1 && ccw(S[S.size() - 2], S[S.size() - 1], P[i]) <= 0)
-			S.pop_back();
-		S.push_back(P[i]);
-	}
-	while (S.size() > 1 && S.back().x == S[S.size() - 2].x) S.pop_back();
-	P = S;
-	int cnt = 100; while (cnt--) {
-
+	std::cin >> T;
+	while (T--) {
+		int p, q, a = 0, b = 0;
+		std::cin >> p >> q >> N;
+		Polygon P(N); for (Pos& p : P) std::cin >> p;
+		norm(P);
+		for (int i = 0; i < N; i++) P[i].i = i;
+		Polygon H = graham_scan(P);
+		int sz = H.size();
+		for (int i = 0; i < sz; i++) {
+			if (P[i].i == (P[(i + 1) % sz].i - 1 + N) % N) b++;
+			else a++;
+		}
+		std::cout << std::max(b * q - a * p, 0) << "\n";
 	}
 	return;
 }

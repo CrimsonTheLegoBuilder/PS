5fcca0fecd8a56db2ce2e2495842588213404e4f
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-10-20 08:11:57 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -6,37 +6,39 @@
 #include <cassert>
 #include <vector>
 typedef long long ll;
-//typedef long double ld;
 typedef double ld;
-const int LEN = 1e5;
-inline int sign(ll x) { return x < 0 ? -1 : !!x; }
+const ll INF = 1e17;
+const int LEN = 1e5 + 1;
+inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 
-#define STRONG 0
-#define WEAK 1
-
-int N;
+int N, T;
 struct Pos {
 	int x, y, i;
 	Pos(int x_ = 0, int y_ = 0, int i_ = -1) : x(x_), y(y_), i(i_) {}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
 	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
 	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
 	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
 	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
 	ll Euc() const { return (ll)x * x + (ll)y * y; }
 	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
-} s, e;
+}; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
 int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
 ll area(Polygon& H) {
 	ll a = 0; int sz = H.size();
 	for (int i = 0; i < sz; i++) a += H[i] / H[(i + 1) % sz];
 	return a;
 }
+void norm(Polygon& H) { if (area(H) < 0) std::reverse(H.begin(), H.end()); }
 Polygon graham_scan(Polygon& C) {
 	Polygon H;
 	if (C.size() < 3) {
@@ -59,57 +61,26 @@ Polygon graham_scan(Polygon& C) {
 	}
 	return H;
 }
-Polygon monotone_chain(Polygon& C) {
-	Polygon H;
-	std::sort(C.begin(), C.end());
-	int sz = C.size();
-	if (sz <= 2) return C;
-	bool f = 1;
-	for (int i = 1; i < sz - 1; i++) if (ccw(C[i - 1], C[i], C[i + 1])) { f = 0; break; }
-	if (f) return C;
-	for (int i = 0; i < C.size(); i++) {
-		while (H.size() > 1 && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) < 0)
-			H.pop_back();
-		H.push_back(C[i]);
-	}
-	H.pop_back();
-	int s = H.size() + 1;
-	for (int i = C.size() - 1; i >= 0; i--) {
-		while (H.size() > s && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) < 0)
-			H.pop_back();
-		H.push_back(C[i]);
-	}
-	H.pop_back();
-	return H;
-}
-bool query() {
-	std::cin >> N;
-	if (!N) return 0;
-	Polygon P(N);
-	for (int i = 0; i < N; i++) std::cin >> P[i];
-	int cnt = 0;
-	while (1) {
-		N = P.size();
-		for (int i = 0; i < N; i++) P[i].i = i;
-		Polygon H = monotone_chain(P);
-		if (!area(H) || H.size() < 3) break;
-		cnt++;
-		if (H.size() == N) break;
-		std::vector<bool> F(N, 0);
-		for (const Pos& p : H) F[p.i] = 1;
-		Polygon C;
-		for (const Pos& p : P) if (!F[p.i]) C.push_back(p);
-		P = C;
-	}
-	std::cout << ((cnt & 1) ? "Take this onion to the lab!\n" : "Do not take this onion to the lab!\n");
-	return 1;
-}
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(9);
-	while (query());
+	std::cin >> T;
+	while (T--) {
+		int p, q, a = 0, b = 0;
+		std::cin >> p >> q >> N;
+		Polygon P(N); for (Pos& p_ : P) std::cin >> p_;
+		norm(P);
+		for (int i = 0; i < N; i++) P[i].i = i;
+		Polygon H = graham_scan(P);
+		int sz = H.size();
+		for (int i = 0; i < sz; i++) {
+			if (P[i].i == (P[(i + 1) % sz].i + 1) % N) b++;
+			else a++;
+		}
+		std::cout << std::max((ll)b * (ll)q - (ll)a * (ll)p, 0ll) << "\n";
+	}
 	return;
 }
 int main() { solve(); return 0; }

be0efd0d996d12617f2aff7585636412901dd709
Jay <wnwoghd22@gmail.com>
2025-11-08 09:44:40 +0900

    25

---
+++ BOJ/src/boj2397.cpp
@@ -0,0 +1,148 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <random>
+#include <array>
+#include <tuple>
+typedef long long ll;
+//typedef long double ld;
+typedef double ld;
+typedef std::pair<int, int> pi;
+typedef std::vector<int> Vint;
+typedef std::vector<ld> Vld;
+const ll INF = 1e17;
+const int LEN = 1e3 + 1;
+const ld TOL = 1e-7;
+const ll MOD = 1e9 + 7;
+const ld PI = acos(-1);
+inline int sign(const int& x) { return x < 0 ? -1 : !!x; }
+inline int sign(const ll& x) { return x < 0 ? -1 : !!x; }
+inline bool zero(const ll& x) { return !x; }
+inline ll sq(const ll& x) { return x * x; }
+inline int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+
+#define LO x
+#define HI y
+
+#define LINE 1
+#define CIRCLE 2
+
+#define STRONG 0
+#define WEAK 1
+
+int N, M, T, Q;
+struct Pos {
+	int x, y;
+	Pos(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	bool operator <= (const Pos& p) const { return x == p.x ? y <= p.y : x <= p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const int& n) const { return { x * n, y * n }; }
+	Pos operator / (const int& n) const { return { x / n, y / n }; }
+	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
+	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos operator ^ (const Pos& p) const { return { x * p.x, y * p.y }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos& operator *= (const int& n) { x *= n; y *= n; return *this; }
+	Pos& operator /= (const int& n) { x /= n; y /= n; return *this; }
+	Pos operator - () const { return { -x, -y }; }
+	Pos operator ~ () const { return { -y, x }; }
+	Pos operator ! () const { return { y, x }; }
+	ll xy() const { return (ll)x * y; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	int Man() const { return std::abs(x) + std::abs(y); }
+	ld mag() const { return hypot(x, y); }
+	ld rad() const { return atan2(y, x); }
+	friend ld rad(const Pos& p1, const Pos& p2) { return atan2l(p1 / p2, p1 * p2); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+const Pos INVAL = Pos(-1, -1);
+typedef std::vector<Pos> Polygon;
+Pos seg[LEN][2];
+Pos B[LEN][12];
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ll dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return sign(cross(d1, d2, d3, d4)); }
+ld projection(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d1) / (d2 - d1).mag(); }
+ld projection(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3) / (d2 - d1).mag(); }
+bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) >= 0; }
+bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0; }
+int collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return !ccw(d1, d2, d3) && !ccw(d1, d2, d4); }
+bool between(const Pos& d0, const Pos& d1, const Pos& q) { return sign(dot(d0, d1, q)) < 0 && sign(dot(d1, d0, q)) < 0; }
+bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2, const int& f = STRONG) {
+	bool f1 = ccw(s1, s2, d1) * ccw(s2, s1, d2) > 0;
+	bool f2 = ccw(d1, d2, s1) * ccw(d2, d1, s2) > 0;
+	if (f == WEAK) return f1 && f2;
+	bool f3 = on_seg_strong(s1, s2, d1) ||
+		on_seg_strong(s1, s2, d2) ||
+		on_seg_strong(d1, d2, s1) ||
+		on_seg_strong(d1, d2, s2);
+	return (f1 && f2) || f3;
+}
+bool inside(const Pos& p0, const Pos& p1, const Pos& p2, const Pos& q, const int& f = STRONG) {
+	if (ccw(p0, p1, p2) < 0) return ccw(p0, p1, q) >= f || ccw(p1, p2, q) >= f;
+	return ccw(p0, p1, q) >= f && ccw(p1, p2, q) >= f;
+}
+bool inner_check(const Polygon& H, const Pos& q) {
+	int sz = H.size();
+	for (int i = 0, j; i < sz; i++) {
+		j = (i + 1) % sz;
+		if (ccw(H[i], H[j], q) < 0) return 0;
+	}
+	return 1;
+}
+bool block(const int& i, const int& j) {
+	bool f0 = intersect(seg[i][0], O, seg[j][0], seg[j][1], WEAK);
+	bool f1 = intersect(seg[i][1], O, seg[j][0], seg[j][1], WEAK);
+	if (f0 || f1) return 1;
+	f0 = intersect(seg[i][0], O, seg[j][0], seg[j][1]);
+	f1 = intersect(seg[i][1], O, seg[j][0], seg[j][1]);
+	if (f0 && f1) return 1;
+	return 0;
+}
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(15);
+	std::cin >> N;
+	for (int i = 0; i < N; i++) {
+		int x, y, l; std::cin >> x >> y >> l;
+		Polygon box = { Pos(x, y), Pos(x + l, y), Pos(x + l, y + l), Pos(x, y + l) };
+		Pos s = box[0], e = box[0];
+		for (const Pos& b : box) {
+			if (ccw(O, s, b) < 0) s = b;
+			if (ccw(O, e, b) > 0) e = b;
+		}
+		seg[i][0] = s;
+		seg[i][1] = e;
+		B[i][0] = box[0];
+		B[i][1] = box[1];
+	}
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		bool f = 1;
+		for (int j = 0; j < N; j++) {
+			if (i == j) continue;
+			if (block(i, j)) { f = 0; break; }
+		}
+		std::cout << "f:: " << f << "\n";
+		cnt += f;
+	}
+	std::cout << cnt << "\n";
+	return;
+}
+int main() { solve(); return 0; }

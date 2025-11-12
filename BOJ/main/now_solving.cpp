63d05e440d1866b862dcc572174359d5455b3967
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-11-12 11:15:48 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -91,7 +91,7 @@ ld intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2, cons
 	ld a1 = ((q2 - q1) / (q1 - p1)) / det;
 	ld a2 = ((p2 - p1) / (p1 - q1)) / -det;
 	return a1;
-	//if (0 < a1 && a1 < 1 && -TOL < a2 && a2 < 1 + TOL) return a1;
+	//if (-TOL < a1 && a1 < 1 + TOL && -TOL < a2 && a2 < 1 + TOL) return a1;
 	//return -1;
 }
 bool circle_inner_check(const Pos& q, const ll& r, const Pos& p) { return sq(r) >= (p - q).Euc(); }

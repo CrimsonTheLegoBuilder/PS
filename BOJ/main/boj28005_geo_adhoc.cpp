da92c28e7407fdc0c7c5120f9740ecbfcdf36226
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-11-22 17:30:39 +0900

    25

--- BOJ/main/boj28005_geo_adhoc.cpp
+++ BOJ/main/boj28005_geo_adhoc.cpp
@@ -33,7 +33,8 @@ inline ll sq(const ll& x) { return x * x; }
 int N, M, T, Q;
 struct Pos {
 	int x, y;
-	Pos(int x_ = 0, int y_ = 0) : x(x_), y(y_) {}
+	int i;
+	Pos(int x_ = 0, int y_ = 0, int i_ = -1) : x(x_), y(y_), i(i_) {}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
 	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
@@ -44,6 +45,7 @@ struct Pos {
 	Pos operator / (const int& n) const { return { x / n, y / n }; }
 	ll operator * (const Pos& p) const { return (ll)x * p.x + (ll)y * p.y; }
 	ll operator / (const Pos& p) const { return (ll)x * p.y - (ll)y * p.x; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
 	Pos operator - () const { return { -x, -y }; }
 	Pos operator ~ () const { return { -y, x }; }
 	Pos operator ! () const { return { y, x }; }
@@ -53,7 +55,14 @@ struct Pos {
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = Pos(0, 0);
 const Pos INVAL = Pos(-1, -1);
+Pos ep;//point on event line
 typedef std::vector<Pos> Polygon;
+bool cmpt(const Pos& u, const Pos& v) {
+	bool f0 = O < u;
+	bool f1 = O < v;
+	if (f0 != f1) return f1;
+	return u / v > 0;	
+}
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
 ll cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
 ll dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
@@ -74,18 +83,226 @@ bool intersect(const Pos& s1, const Pos& s2, const Pos& d1, const Pos& d2, const
 		on_seg_strong(d1, d2, s2);
 	return (f1 && f2) || f3;
 }
-
+Polygon monotone_chain(Polygon C) {
+	Polygon H;
+	std::sort(C.begin(), C.end());
+	C.erase(unique(C.begin(), C.end()), C.end());
+	if (C.size() <= 2) { for (const Pos& pos : C) H.push_back(pos); }
+	else {
+		for (int i = 0; i < C.size(); i++) {
+			while (H.size() > 1 && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) < 0)
+				H.pop_back();
+			H.push_back(C[i]);
+		}
+		H.pop_back();
+		int s = H.size() + 1;
+		for (int i = C.size() - 1; i >= 0; i--) {
+			while (H.size() > s && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) < 0)
+				H.pop_back();
+			H.push_back(C[i]);
+		}
+		H.pop_back();
+	}
+	return H;
+}
 struct Seg {
 	Pos s, e;
-};
+	int i, rvs;
+	Seg(Pos s_ = Pos(), Pos e_ = Pos(), int i_ = -1, int rvs_ = 0) :
+		s(s_), e(e_), i(i_), rvs(rvs_) {
+		if (s.y > e.y) {
+			std::swap(s, e);
+			rvs = 1;
+		}
+	}
+	bool operator<(const Seg& o) const {
+		if (s == o.s) return e.x < o.e.x;
+		if (e == o.e) return s.x < o.s.x;
+		return ccw(s, e, o.s);
+	}
+	bool operator==(const Seg& o) const {return s == o.s && e == o.e; }
+} seg[LEN];
+typedef std::vector<Seg> Segs;
+struct Trep {
+	Seg l, r;
+	int b, u;
+	Trep(Seg l_, Seg r_, int b_ = -1, int u_ = -1) : l(l_), r(r_), b(b_), u(u_) {}
+} room[LEN << 2];
+class SplayTree {
+	struct Node {
+		Node* l;
+		Node* r;
+		Node* p;
+		Seg key;
+		Node(Seg s) : l(0), r(0), p(0) { key = s; }
+		~Node() { if (l) delete l; if (r) delete r; }
+	} *root;
+	void rotate(Node* x) {
+		Node* p = x->p;
+		if (!p) return;
+		Node* b = 0;
+		if (x == p->l) {
+			p->l = b = x->r;
+			x->r = p;
+		}
+		else {
+			p->r = b = x->l;
+			x->l = p;
+		}
+		x->p = p->p;
+		p->p = x;
+		if (b) b->p = p;
+		(x->p ? p == x->p->l ? x->p->l : x->p->r : root) = x;
+	}
+	void splay(Node* x) {
+		while (x->p) {
+			Node* p = x->p;
+			Node* g = p->p;
+			if (g) {
+				if ((x == p->l) == (p == g->l)) rotate(p);
+				else rotate(x);
+			}
+			rotate(x);
+		}
+	}
+	Node* find(Seg s) {
+		if (!root) return 0;
+		Node* p = root;
+		while (1) {
+			if (p->key == s/* eq */) break;
+			if (p->key < s/* cmp */) {
+				if (!p->r) {
+					break;
+				}
+				p = p->r;
+			}
+			else {
+				if (!p->l) {
+					break;
+				}
+				p = p->l;
+			}
+		}
+		splay(p);
+		return p;
+	}
 
+public:
+	SplayTree() : root(0) {}
+	~SplayTree() { if (root) delete root; }
+	void insert(Seg s) {
+		if (!root) {
+			root = new Node(s);
+			return;
+		}
+		Node* p = root;
+		Node** pp;
+		while (1) {
+			if (p->key < s/* cmp */) {
+				if (!p->r) {
+					pp = &p->r;
+					break;
+				}
+				p = p->r;
+			}
+			else {
+				if (!p->l) {
+					pp = &p->l;
+					break;
+				}
+				p = p->l;
+			}
+		}
+		Node* x = new Node(s);
+		*pp = x;
+		x->p = p;
+		splay(x);
+	}
+	void pop(Node* ptr) {
+		if (!ptr) return;
+		splay(ptr);
+		Node* p = root;
+		if (p->l && p->r) {
+			root = p->l; root->p = 0;
+			Node* l = root;
+			while (l->r) l = l->r;
+			l->r = p->r;
+			p->r->p = l;
+		}
+		else if (p->l) root = p->l, root->p = 0;
+		else if (p->r) root = p->r, root->p = 0;
+		else root = 0;
+		p->l = p->r = 0;
+		delete p;
+	}
+	Node* get_prev(Node* x) {
+		if (!x || !x->l) return nullptr;
+		Node* p = x->l;
+		while (p->r) p = p->r;
+		return p;
+	}
+	Node* get_next(Node* x) {
+		if (!x || !x->r) return nullptr;
+		Node* p = x->r;
+		while (p->l) p = p->l;
+		return p;
+	}
+} sp;
+ll count(const Polygon& P) {
+	int sz = P.size();
+	for (int i = 0; i < sz; i++) {
+		//Seg? Node?
+		//해당 점의 왼쪽 변 찾기
+		Segs B, U;
+		for (int j = i; j < N; j++) {
+			//막혀있는지 판단
+			//오른쪽벽 찾기
+			//아래쪽 변을 찾으면 B에 넣기
+			//위쪽 변을 찾으면 U에 넣기
+			//막혀있다면 break;
+		}
+		//아래쪽 변과 위쪽 변은 순서대로 정렬되어있는 형태이며, 아래쪽 변에는 현재 사다리꼴 방의 번호를 저장
+		//위쪽 방은 현재 가장 번호가 큰 방 이후의 방 번호를 부여, 각 왼쪽 벽에 방 번호를 부여 (seg 객체를 사용)
+		//가장 왼쪽 변은 -1, 가장 오른쪽 변은 INF, 나머지 공선점들은 어차피 모든 점이 정수 좌표이므로 각 방의 시작과 끝점을 정수 이벤트로 정렬 후
+		//각각의 엇갈린 이벤트(방)들을 그래프로 연결
+		//아래쪽 변들은 전부 sp에서 제거
+		//위쪽 변들은 sp에 넣기
+	}
+	//그래프 완성 후 u region 세서 반환
+	return 0;
+}
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(8);
-	std::cin >> N; Polygon P(N); for (Pos& p : P) std::cin >> p;
-	
+	int i = 0;
+	std::cin >> N; Polygon P(N); for (Pos& p : P) std::cin >> p, p.i = i, i++;
+	Polygon C, F; Pos cur = O;
+	for (int i = 0; i < N; i++) { C.push_back(P[(i + 1) % N] - P[i]); }
+	std::sort(C.begin(), C.end(), cmpt);
+	F = { cur };
+	for (int i = 0; i < N; i++) {
+		cur += C[i];
+		while (i < N - 1 && C[i] / C[i + 1] == 0) {
+			cur += C[i + 1];
+		}
+		F.push_back(cur);
+	}
+	for (int _ = 0; _ < 2; _++) {
+		int t = -INF;
+		for (int i = 0; i < N; i++) P[i].i = i, t = std::max(t, P[i].y);
+		Polygon H = monotone_chain(P);
+		int sz = H.size(), UG = 0;
+		for (int i = 0; i < sz; i++) {
+			if ((H[i].i + 1) % N != H[(i + 1) % sz].i) {
+				int s = H[i].i, e = H[(i + 1) % sz].i;
+				//오목한 다각형 획득 후 스위핑
+			}
+		}
+
+		for (Pos& p : P) p.y += UG/* ??? */, p = ~p;
+	}
 	return;
 }
-int main() { solve(); return 0; }//boj34702
+int main() { solve(); return 0; }//boj28005

9ab3f682a601ab89fbc625cf0478a93dc4f66d84
Jay <wnwoghd22@gmail.com>
2025-11-23 23:03:37 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -17,26 +17,7 @@ void solve() {
 	std::cout.tie(0);
 	std::cout << std::fixed;
 	std::cout.precision(8);
-	int h0, m0, s0, t0 = 0;
-	scanf("%d:%d:%d", &h0, &m0, &s0);
-	t0 += h0 * 3600;
-	t0 += m0 * 60;
-	t0 += s0;
-	//printf("t0: %d\n", t0);
-	int h1, m1, s1, t1 = 0;
-	scanf("%d:%d:%d", &h1, &m1, &s1);
-	t1 += h1 * 3600;
-	t1 += m1 * 60;
-	t1 += s1;
-	//printf("t1: %d\n", t1);
-	int t, k;
-	scanf("%d %d", &t, &k);
-	N = t1 - t0;
-	if (N < 0) { printf("0\n"); return; }
-	//printf("N: %d\n", N);
-	int ans = 1;
-	if (N < (t * (100 - k)) / 100) ans = 0;
-	printf("%d\n", ans);
+
 	return;
 }
-int main() { solve(); return 0; }//boj34702
+int main() { solve(); return 0; }

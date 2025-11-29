a627cc355ef6064804d322e104d2ef84266f3a39
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-11-29 12:17:32 +0900

    25

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -1,56 +1,92 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+#include <deque>
+typedef long long ll;
+typedef double ld;
+const ll INF = 1e17;
+const int LEN = 1e6 + 1;
+const ld TOL = 1e-7;
+bool zero(const ld& x) { return std::abs(x) < TOL; }
+int sign(const ld& x) { return x < -TOL ? -1 : x > TOL; }
+int sign(const ll& x) { return x < 0 ? -1 : !!x; }
 
-int main() {
-    int stack[100000] = { 0, };
-    int array[100000] = { 0, };
-    char act[100000];
-    int N, top = -1, element = 1;
-    scanf("%d", &N);
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &array[i]);
-    }
-    int i = 0;
-    int tmp = 0;
-    while (1) {
-        if (i < N) {
-            if (array[i] < element) {
-                if (stack[top] != array[i]) {
-                    printf("NO\n");
-                    break;
-                }
-            }
-            while (array[i] >= element) {
-                stack[++top] = element++;
-                act[tmp++] = '+';
+int N, M, Q, T;
+struct Pos {
+	int x, y;
+	Pos(int X = 0, int Y = 0) : x(X), y(Y) {}
+	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
+	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
+	bool operator < (const Pos& p) const { return x == p.x ? y < p.y : x < p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	ll operator * (const Pos& p) const { return { (ll)x * p.x + (ll)y * p.y }; }
+	ll operator / (const Pos& p) const { return { (ll)x * p.y - (ll)y * p.x }; }
+	ll Euc() const { return (ll)x * x + (ll)y * y; }
+	ld mag() const { return hypot(x, y); }
+	inline friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	inline friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+ll cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(9);
+	int x, yd, yu;
+	Pos s, e;
+	std::cin >> M >> N;
+	s.x = 0;
+	s.y = 0;
+	e.x = 0;
+	e.y = N + 1;
+	std::vector<Pos> U(N), D(N);
+	for (int i = 1; i <= N; i++) {
+		std::cin >> yu >> yd;
+		U[i - 1] = Pos(yu, i);
+		D[i - 1] = Pos(yd, i);
+	}
+	std::deque<Pos> HU, HD;
+	HU.push_back(s); HU.push_back(U[0]);
+	U.push_back(e);
+	HD.push_back(s); HD.push_back(D[0]);
+	D.push_back(e);
 
-            }
-            if (stack[top] == array[i]) {
-                i++;
-                act[tmp++] = '-';
-
-                stack[top--] = 0;
-            }
-            else if (stack[top] != array[i]) {
-                if (top == -1) break;
-                if (i >= N) {
-                    printf("NO\n");
-                    break;
-                }
-            }
-        }
-        else {
-            if (top == -1) {
-                for (int n = 0; n < tmp; n++) {
-                    printf("%c\n", act[n]);
-                }
-                break;
-            }
-            else {
-                printf("NO\n");
-                break;
-            }
-        }
-    }
-    return 0;
+	std::vector<Pos> ret;
+	ret.push_back(s);
+	for (int i = 1; i <= N; i++) {
+		while (HU.size() > 1 && ccw(HU[HU.size() - 2], HU.back(), U[i]) <= 0) HU.pop_back();
+		HU.push_back(U[i]);
+		while (HD.size() > 1 && ccw(HD[HD.size() - 2], HD.back(), D[i]) >= 0) HD.pop_back();
+		HD.push_back(D[i]);
+		if (HU.size() <= 2 && HD.size() <= 2) continue;
+		else if (HU.size() == 2) {
+			if (ccw(HU[0], HU[1], HD[1]) <= 0) continue;
+			while (HD.size() > 1 && ccw(HD[0], HD[1], HU[1]) <= 0) ret.push_back(HD[0]), HD.pop_front();
+			ret.push_back(HD[0]);
+			HU[0] = HD[0];
+		}
+		else if (HD.size() == 2) {
+			if (ccw(HD[0], HD[1], HU[1]) >= 0) continue;
+			while (HU.size() > 1 && ccw(HU[0], HU[1], HD[1]) >= 0) ret.push_back(HU[0]), HU.pop_front();
+			ret.push_back(HU[0]);
+			HD[0] = HU[0];
+		}
+	}
+	ret.push_back(e);
+	//std::sort(ret.begin(), ret.end());
+	ret.erase(unique(ret.begin(), ret.end()), ret.end());
+	//for (const Pos& p : ret) std::cout << p << "\n";
+	int sz = ret.size();
+	ld d = 0;
+	for (int i = 0; i < sz - 1; i++) d += (ret[i] - ret[i + 1]).mag();
+	std::cout << d << "\n";
+	return;
 }
+int main() { solve(); return 0; }//GCPC 2021 F Flappy Bird boj25250

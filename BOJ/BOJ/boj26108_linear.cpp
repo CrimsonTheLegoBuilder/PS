0df2885ba1b26e540dc209a1474a9c07486633d7
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-12-14 00:30:30 +0900

    25

--- BOJ/BOJ/boj26108_linear.cpp
+++ BOJ/BOJ/boj26108_linear.cpp
@@ -5,6 +5,7 @@
 #include <cstring>
 #include <cassert>
 #include <vector>
+#include <queue>
 typedef long long ll;
 //typedef long double ld;
 typedef double ld;
@@ -56,7 +57,7 @@ struct Pos {
 	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
 }; const Pos O = Pos(0, 0);
 typedef std::vector<Pos> Polygon;
-Polygon H[K_];
+Polygon H[K_], LH[K_], UH[K_];
 bool cmpx(const Pos& p, const Pos& q) { return p.x == q.x ? p.y < q.y : p.x < q.x; }
 bool cmpx_rvs(const Pos& p, const Pos& q) { return p.x == q.x ? p.y > q.y : p.x < q.x; }
 bool cmpy(const Pos& p, const Pos& q) { return p.y == q.y ? p.x < q.x : p.y < q.y; }
@@ -100,7 +101,14 @@ Polygon monotone_chain(Polygon& C) {
 	}
 	return H;
 }
-int top[K_], bot[K_], lb[K_], rb[K_];//convex hull rotating calipers jaw data
+int bot[K_], top[K_], lb[K_], rb[K_];//convex hull rotating calipers jaw data
+struct Event {
+	Pos v;
+	int h, i;
+	Event(Pos v_ = Pos(), int h_ = -1, int i_ = -1) : v(v_), h(h_), i(i_) {}
+	bool operator < (const Event& o) const { return v / o.v > 0; }
+};
+std::priority_queue<Event> vpq, epq, bpq, tpq;
 void solve() {
 	std::cin.tie(0)->sync_with_stdio(0);
 	std::cout.tie(0);
@@ -127,12 +135,37 @@ void solve() {
 	for (int i = 0, j; i < K + 1; i++) {
 		j = N - i - 1;
 		bot[i] = P[i].i;
-		top[i] = P[j].i;
+		top[i] = P[j].i;//No need to record. no effect rotating calipers
+	}
+	Pos b = bot[K];
+	Pos t = top[K];
+	for (int h = 0; h < K + 1; h++) {
+		int sz = H[h].size();
+
+		for (int i = 0; i < sz; i++) {
+
+		}
 	}
 	Pos main_vec = Pos(0, -1);
-	Pos vbot = Pos(0, -1), vtop = Pos(0, 1);
-	while (1) {
-
+	Pos cur = Pos(0, -1), vbot = Pos(0, -1), vtop = Pos(0, 1);
+	int cnt = 3e7;
+	while (cnt--) {//O(NKlogK)
+		//새로 시작하면 현재 이벤트 벡터 하나 깐다.
+		//반바퀴 다 돌았으면 나간다.
+		//회전에 의해 점이 새로 들어오고 나간다. 이를 처리해야 한다.
+		//정렬 후 새로 들어올 가능성이 있는 점과 맨 뒤에 있는 점에 대해 현재 이벤트 벡터와의 방향성을 검사한다.
+		//맨 뒤에 있는 점이 더 뒤에 있다고 판단한다면 점을 방출한 후 새로 넣는다.
+		//아마도? 1대1 교환이 이루어지긴 할 것임? 새로 들어올 후보 점을 다시 한 번 검사해서 또 들어올 수 있는지를 검사해야 한다.
+		//이벤트 벨트 안에 점이 정확히 K개 제외되는지 엄밀한 검사를 수행하는 로직이 필요함
+		//앞대가리에 있는 각 껍질의 점들을 바로 O(1)에 조회 가능한 방법이 필요함.
+		//한 번에 기록용 배열을 4개 가지고 있어야 하고, 메인 이벤트 말고 이벤트 앞대가리도 정렬이 되는 방식으로 구현을 해야할 수 있음
+		//이벤트야 벡터로 저장되니까 그려러니 하는데 앞에서 들어올 후보 점을 판단하는 로직은 어떻게 단순화를 시켜야할까?
+		//기울기가 같은 이벤트 벡터를 전부 힙에서 깐다.
+		//이벤트 벡터에서 읽어온 순서가 변하는 두 점들을 전부 처리한다.
+		//이벤트 벡터에서 읽어온 변한 순서의 점들을 대상으로 K 개 안에 있는 점들에 대해 계산한다.
+		//가장 위와 아래에 있는 두 점의 이벤트 벡터 수직 벡터 사영 성분 간 길이의 절반으로 최소값을 갱신한다.
+		//순서가 바뀐 두 점의 앞, 뒤를 새로 검사해서 이벤트를 새로 삽입한다. 힙에 넣으므로 자동으로 정렬된다.
+		//새로 넣는 이벤트는 현재 기울기보다 무조건 정렬 순서 상 큰 기울기를 가져야 한다.
 	}
 	return;
 }

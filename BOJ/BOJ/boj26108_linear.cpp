fbf8f0761135ff88ee1d0e839d11af9aeed2a507
CrimsonTheLegoBuilder <redcube231@gmail.com>
2025-12-16 11:13:44 +0900

    25

--- BOJ/BOJ/boj26108_linear.cpp
+++ BOJ/BOJ/boj26108_linear.cpp
@@ -51,8 +51,8 @@ int N, K;
 struct Pos {
 	int x, y;
 	//ll x, y;
-	int h, i, j;
-	Pos(int x_ = 0, int y_ = 0, int h_ = -1, int i_ = -1, int j_ = -1) : x(x_), y(y_), h(h_), i(i_), j(j_) {}
+	int pi, hi, i;
+	Pos(int x_ = 0, int y_ = 0, int p_ = -1, int h_ = -1, int i_ = -1) : x(x_), y(y_), pi(p_), hi(h_), i(i_) {}
 	//Pos(ll x_ = 0, ll y_ = 0) : x(x_), y(y_) {}
 	bool operator == (const Pos& p) const { return x == p.x && y == p.y; }
 	bool operator != (const Pos& p) const { return x != p.x || y != p.y; }
@@ -93,28 +93,50 @@ bool on_seg_strong(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1
 bool on_seg_weak(const Pos& d1, const Pos& d2, const Pos& d3) { return !ccw(d1, d2, d3) && dot(d1, d3, d2) > 0; }
 int collinear(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return !ccw(d1, d2, d3) && !ccw(d1, d2, d4); }
 bool between(const Pos& d0, const Pos& d1, const Pos& q) { return sign(dot(d0, d1, q)) < 0 && sign(dot(d1, d0, q)) < 0; }
-Polygon monotone_chain(Polygon& C, int t = WHOLE, bool s = NO_SORT) {
-	if (s == SORT) { std::sort(C.begin(), C.end()); C.erase(unique(C.begin(), C.end()), C.end()); }
-	Polygon L, U;
-	if (t != UPPER) {
+//Polygon monotone_chain(Polygon& C, int t = WHOLE, bool s = NO_SORT) {
+//	if (s == SORT) { std::sort(C.begin(), C.end()); C.erase(unique(C.begin(), C.end()), C.end()); }
+//	Polygon L, U;
+//	if (t != UPPER) {
+//		for (int i = 0; i < C.size(); i++) {
+//			while (L.size() > 1 && ccw(L[L.size() - 2], L[L.size() - 1], C[i]) <= 0)
+//				L.pop_back();
+//			L.push_back(C[i]);
+//		}
+//		if (t == LOWER) return L;
+//	}
+//	if (t != LOWER) {
+//		for (int i = C.size() - 1; i >= 0; i--) {
+//			while (U.size() > 1 && ccw(U[U.size() - 2], U[U.size() - 1], C[i]) <= 0)
+//				U.pop_back();
+//			U.push_back(C[i]);
+//		}
+//		if (t == UPPER) return U;
+//	}
+//	L.pop_back(); U.pop_back();
+//	L.insert(L.end(), U.begin(), U.end());
+//	return L;
+//}
+Polygon monotone_chain(Polygon& C) {
+	Polygon H;
+	std::sort(C.begin(), C.end());
+	C.erase(unique(C.begin(), C.end()), C.end());
+	if (C.size() <= 2) { for (const Pos& pos : C) H.push_back(pos); }
+	else {
 		for (int i = 0; i < C.size(); i++) {
-			while (L.size() > 1 && ccw(L[L.size() - 2], L[L.size() - 1], C[i]) <= 0)
-				L.pop_back();
-			L.push_back(C[i]);
+			while (H.size() > 1 && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
+				H.pop_back();
+			H.push_back(C[i]);
 		}
-		if (t == LOWER) return L;
-	}
-	if (t != LOWER) {
+		H.pop_back();
+		int s = H.size() + 1;
 		for (int i = C.size() - 1; i >= 0; i--) {
-			while (U.size() > 1 && ccw(U[U.size() - 2], U[U.size() - 1], C[i]) <= 0)
-				U.pop_back();
-			U.push_back(C[i]);
+			while (H.size() > s && ccw(H[H.size() - 2], H[H.size() - 1], C[i]) <= 0)
+				H.pop_back();
+			H.push_back(C[i]);
 		}
-		if (t == UPPER) return U;
+		H.pop_back();
 	}
-	L.pop_back(); U.pop_back();
-	L.insert(L.end(), U.begin(), U.end());
-	return L;
+	return H;
 }
 struct Event {
 	Pos v;
@@ -202,7 +224,7 @@ struct Jaw {
 		}
 		if (candidate_insert_check(cur)) {//O(NK)
 			E[K] = C[0];
-			int h = E[K].h, i = E[i].i;
+			int h = E[K].hi, i = E[i].i;
 			candidate_update(H[h], i, cur);
 		}
 		return f;
@@ -222,25 +244,38 @@ struct Calipers {
 		Vbool F(N, 0);
 		for (int k = 0; k <= K; k++) {//O(NK)
 			Polygon tmp;
-			for (const Pos& q : Q) if (!F[q.i]) tmp.push_back(q);
+			for (const Pos& q : Q) if (!F[q.pi]) tmp.push_back(q);
 			if (tmp.empty()) break;
 			h++;
-			H[k] = monotone_chain(tmp, WHOLE);
-			L[k] = monotone_chain(tmp, LOWER);
-			U[k] = monotone_chain(tmp, UPPER);
-			for (const Pos& h : H[k]) F[h.i] = 1;
+			H[k] = monotone_chain(tmp);
+			//H[k] = monotone_chain(tmp, WHOLE);
+			//L[k] = monotone_chain(tmp, LOWER);
+			//U[k] = monotone_chain(tmp, UPPER);
+			int sz = H[k].size();
+			for (int i = 0; i < sz; i++) H[k][i].hi = k, H[k][i].i = i;
+			int b = H[k][0].i;
+			for (int i = 0; i < sz; i++) if (H[k][b] < H[k][i]) b = i;
+			for (int i = 0; i <= b; i++) L[k].push_back(H[k][i]);
+			for (int i = b; i < sz; i++) U[k].push_back(H[k][i]);
+			for (const Pos& h : H[k]) F[h.pi] = 1;
 			Q.clear();
-			for (const Pos& t : tmp) if (!F[t.i]) Q.push_back(t);
+			for (const Pos& t : tmp) if (!F[t.pi]) Q.push_back(t);
 		}
 		Q.clear(); for (int i = 0; i < N; i++) if (F[i]) Q.push_back(P[i]);
 		std::sort(Q.begin(), Q.end(), cmpx_rvs);
 		bot.init(Q, L, N, K, s); top.init(Q, U, N, K, e);
 	}
-	void rotate() {
-		Pos nxt;
-		//all pq top search
-		cur = nxt;
-		return;
+	bool rotate() {
+		Polygon V;
+		if (bot.EQ.size()) V.push_back(bot.EQ.top().v);
+		if (bot.CQ.size()) V.push_back(bot.CQ.top().v);
+		if (top.EQ.size()) V.push_back(-top.EQ.top().v);
+		if (bot.CQ.size()) V.push_back(-top.CQ.top().v);
+		if (V.empty()) { cur = Pos(0, 1); return 0; }
+		cur = V[0];
+		int sz = V.size();
+		for (int i = 1; i < sz; i++) if (cur / V[i] < 0) cur = V[i];
+		return 1;
 	}
 	ld dist(int b = -1, int t = -1) {
 
@@ -254,8 +289,7 @@ struct Calipers {
 		for (const Event& hij : ev) {
 			//cal_d
 		}
-		rotate();
-		return 1;
+		return rotate();
 	}
 } C;
 void solve() { 
@@ -265,6 +299,7 @@ void solve() {
 	std::cout.precision(15);
 	std::cin >> N >> K;
 	Polygon P(N); for (Pos& p : P) std::cin >> p;
+	for (int i = 0; i < N; i++) P[i].i = i;
 	C.init(P, N, K, Pos(0, -1));
 	ld ret = INF;
 	while (C.jaw_rotate(ret)) {}//O(NKlogK)

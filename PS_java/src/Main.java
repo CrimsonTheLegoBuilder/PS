d340cd25d51000d31eb531cd1807f765a38b748c
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-01-14 00:54:59 +0900

    java

--- PS_java/src/Main.java
+++ PS_java/src/Main.java
@@ -1,8 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = a + b;
-        System.out.println("answer: " + c);
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        for (int i = 0; i < N; i++) {
+            String s = scan.next();
+            int len = s.length();
+            char l = s.charAt(len / 2 - 1);
+            char r = s.charAt(len / 2);
+            if (l == r) {
+                System.out.println("Do-it");
+            }
+            else {
+                System.out.println("Do-it-Not");
+            }
+        }
     }
 }

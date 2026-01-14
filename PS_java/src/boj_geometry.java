14400e4c0a44e9553448427b730fe0e04f115675
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-01-14 14:21:25 +0900

    javajava

---
+++ PS_java/src/boj_geometry.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class boj_geometry {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        System.out.println("Geometry Fuck!!");
+    }
+}

c3e33115ffec09a1bfc120a85b5eb20500ccb17a
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-01-18 22:58:01 +0900

    java 6/

--- PS_java/src/boj_bronze.java
+++ PS_java/src/boj_bronze.java
@@ -3,18 +3,19 @@
 //import java.io.IOException;
 //import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class boj_bronze {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (true) {
-            int l = sc.nextInt();
-            int w = sc.nextInt();
-            int a = sc.nextInt();
-            if (l == 0 && w == 0 && a == 0) break;
-            if (l == 0) System.out.println(a / w + " " + w + " " + a);
-            if (w == 0) System.out.println(l + " " + a / l + " " + a);
-            if (a == 0) System.out.println(l + " " + w + " " + l * w);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            int[] A = new int[3];
+            for (int j = 0; j < 3; j++) A[j] = sc.nextInt();
+            Arrays.sort(A);
+            int d = A[0] * A[0] + A[1] * A[1];
+            if (d == A[2] * A[2]) System.out.println("Case #" + i + ": YES");
+            else System.out.println("Case #" + i + ": NO");
         }
     }
 }

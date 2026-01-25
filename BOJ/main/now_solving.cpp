da235a7db1eeb3eb25b285f44cf8e5f97532048f
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-01-25 19:38:43 +0900

    23590 1

--- BOJ/main/now_solving.cpp
+++ BOJ/main/now_solving.cpp
@@ -1,318 +1,201 @@
-﻿#include<iostream>
-#include<vector>
-#include<cmath>
-#include<iomanip>
-#include <tuple>
-using namespace std;
-#define pdd pair<long double, long double>
-#define pll pair<long long, long long>
-int n, m, q;
-vector<pll> convex[2];
-long long int area[2][100000];
-int change(long long int tmp) {
-	if (tmp > 0) return 1;
-	if (!tmp) return 0;
+﻿#define _CRT_SECURE_NO_WARNINGS 
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <cstring>
+#include <cassert>
+#include <vector>
+typedef long long ll;
+typedef long double ld;
+//typedef double ld;
+typedef std::vector<int> Vint;
+typedef std::vector<ld> Vld;
+const ld INF = 1e17;
+const ld TOL = 1e-10;
+const ld PI = acos(-1);
+const int LEN = 10005;
+inline int sign(const ld& x) { return x <= -TOL ? -1 : x >= TOL; }
+inline bool zero(const ld& x) { return !sign(x); }
+inline bool eq(const ld& x, const ld& y) { return zero(x - y); }
+inline ll sq(const ll& x) { return x * x; }
+inline ld sq(const ld& x) { return x * x; }
+inline ld fit(const ld& x, const ld& lo, const ld& hi) { return std::min(hi, std::max(lo, x)); }
+inline ld norm(ld th) { while (th < 0) th += 2 * PI; while (sign(th - 2 * PI) >= 0) th -= 2 * PI; return th; }
+
+#define INSIDE 0
+#define MEET 1
+#define OUTSIDE 2
+#define STRONG 0
+#define WEAK 1
+#define LO x
+#define HI y
+
+int T, N, M;
+bool F[3];
+Vld Q;
+Vint sts;
+struct Pos {
+	ld x, y;
+	Pos(ld x_ = 0, ld y_ = 0) : x(x_), y(y_) {}
+	bool operator == (const Pos& p) const { return zero(x - p.x) && zero(y - p.y); }
+	bool operator != (const Pos& p) const { return !zero(x - p.x) || !zero(y - p.y); }
+	bool operator < (const Pos& p) const { return zero(x - p.x) ? y < p.y : x < p.x; }
+	Pos operator + (const Pos& p) const { return { x + p.x, y + p.y }; }
+	Pos operator - (const Pos& p) const { return { x - p.x, y - p.y }; }
+	Pos operator * (const ld& n) const { return { x * n, y * n }; }
+	Pos operator / (const ld& n) const { return { x / n, y / n }; }
+	ld operator * (const Pos& p) const { return { x * p.x + y * p.y }; }
+	ld operator / (const Pos& p) const { return { x * p.y - y * p.x }; }
+	Pos& operator += (const Pos& p) { x += p.x; y += p.y; return *this; }
+	Pos& operator -= (const Pos& p) { x -= p.x; y -= p.y; return *this; }
+	Pos rot(const ld& t) { return { x * cos(t) - y * sin(t), x * sin(t) + y * cos(t) }; }
+	ld Euc() const { return x * x + y * y; }
+	ld mag() const { return sqrt(Euc()); }
+	ld rad() const { return atan2(y, x); }
+	friend std::istream& operator >> (std::istream& is, Pos& p) { is >> p.x >> p.y; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Pos& p) { os << p.x << " " << p.y; return os; }
+}; const Pos O = Pos(0, 0);
+typedef std::vector<Pos> Polygon;
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) / (d3 - d2); }
+ld cross(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) / (d4 - d3); }
+int ccw(const Pos& d1, const Pos& d2, const Pos& d3) { return sign(cross(d1, d2, d3)); }
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3) { return (d2 - d1) * (d3 - d2); }
+ld dot(const Pos& d1, const Pos& d2, const Pos& d3, const Pos& d4) { return (d2 - d1) * (d4 - d3); }
+Pos intersection(const Pos& p1, const Pos& p2, const Pos& q1, const Pos& q2) { ld a1 = cross(q1, q2, p1), a2 = -cross(q1, q2, p2); return (p1 * a2 + p2 * a1) / (a1 + a2); }
+bool inner_check(const Polygon& H, const Pos& p) {
+	int sz = H.size();
+	for (int i = 0; i < sz; i++) if (ccw(H[i], H[(i + 1) % sz], p) < 0) return 0;
+	return 1;
+}
+struct Seg {
+	Pos s, e;
+	Seg(Pos s_ = Pos(), Pos e_ = Pos()) : s(s_), e(e_) {}
+	Pos p(const ld& rt) const { return s + (e - s) * rt; }
+	ld green(const ld& lo = 0, const ld& hi = 1) const {
+		ld d = hi - lo;
+		ld ratio = (lo + hi) * .5;
+		Pos m = p(ratio);
+		return m.y * d * (s.x - e.x);
+	}
+};
+ld intersection(const Seg& s1, const Seg& s2, const bool& f = STRONG) {
+	const Pos& p1 = s1.s, p2 = s1.e, q1 = s2.s, q2 = s2.e;
+	ld det = (q2 - q1) / (p2 - p1);
+	if (zero(det)) return -1;
+	ld a1 = ((q2 - q1) / (q1 - p1)) / det;
+	ld a2 = ((p2 - p1) / (p1 - q1)) / -det;
+	if (f == WEAK) return fit(a1, 0, 1);
+	if (0 < a1 && a1 < 1 && -TOL < a2 && a2 < 1 + TOL) return a1;
 	return -1;
 }
-long long int ccw(pll a, pll b) { return a.first * b.second - a.second * b.first; }
-long long int dist(pll a) { return a.first * a.first + a.second * a.second; }
-long double dccw(pdd a, pdd b) { return a.first * b.second - a.second * b.first; }
-pll tovec(pll a, pll b) { return { b.first - a.first, b.second - a.second }; }
-pdd dtovec(pdd a, pdd b) { return { b.first - a.first, b.second - a.second }; }
-int inconvex(int idx, pll point) {
-	int s = convex[idx].size();
-	pll vec_l = tovec(convex[idx][0], convex[idx][s - 1]);
-	pll vec_r = tovec(convex[idx][0], convex[idx][1]);
-	pll vec_p = tovec(convex[idx][0], point);
-
-	if (ccw(vec_l, vec_p) > 0 || ccw(vec_p, vec_r) > 0) return -1;
-	if (ccw(vec_l, vec_p) == 0) {
-		if (dist(tovec(convex[idx][0], convex[idx][s - 1])) >= dist(tovec(convex[idx][0], point))) return 0;
-		return -1;
+struct Circle {
+	Pos c;
+	ll r;
+	Circle(Pos c_ = Pos(), ll r_ = 0) : c(c_), r(r_) {}
+	bool operator > (const Pos& p) const { return sign(r - (c - p).mag()) > 0; }
+	Pos p(const ld& t) const { return c + Pos(r, 0).rot(t); }
+	ld rad(const Pos& p) const { return (p - c).rad(); }
+	ld area(const ld& lo, const ld& hi) const { return (hi - lo) * r * r * .5; }
+	ld green(const ld& lo, const ld& hi) const {
+		Pos s = Pos(cos(lo), sin(lo)), e = Pos(cos(hi), sin(hi));
+		ld fan = area(lo, hi);
+		Pos m = c + (s + e) * r * (ld).5;
+		ld tz = (cos(lo) - cos(hi)) * m.y * r;
+		return fan + tz - (s / e) * r * r * (ld).5;
 	}
-	if (ccw(vec_p, vec_r) == 0) {
-		if (dist(tovec(convex[idx][0], convex[idx][1])) >= dist(tovec(convex[idx][0], point))) return 0;
-		return -1;
-	}
-	int l = 1, r = s - 1;
-	while (r - l > 1) {
-		int mid = (l + r) / 2;
-		pll vec_m = tovec(convex[idx][0], convex[idx][mid]);
-		if (change(ccw(vec_m, vec_p)) == 1) l = mid;
-		else r = mid;
-	}
-
-	pll vec1 = tovec(convex[idx][l], point);
-	pll vec2 = tovec(point, convex[idx][(l + 1) % s]);
-	return -change(ccw(vec1, vec2));
+	friend std::istream& operator >> (std::istream& is, Circle& p) { is >> p.c.x >> p.c.y >> p.r; return is; }
+	friend std::ostream& operator << (std::ostream& os, const Circle& p) { os << p.c.x << " " << p.c.y << " " << p.r; return os; }
+} C[3];
+Vld intersections(const Circle& a, const Circle& b) {
+	Pos ca = a.c, cb = b.c;
+	Pos vec = cb - ca;
+	ld ra = a.r, rb = b.r;
+	ld distance = vec.mag();
+	ld rd = vec.rad();
+	if (vec.Euc() > sq(ra + rb) + TOL) return {};
+	if (vec.Euc() < sq(ra - rb) - TOL) return {};
+	ld X = (ra * ra - rb * rb + vec.Euc()) / (2 * distance * ra);
+	if (X < -1) X = -1;
+	if (X > 1) X = 1;
+	ld h = acos(X);
+	Vld ret = {};
+	ret.push_back(norm(rd - h));
+	if (zero(h)) return ret;
+	ret.push_back(norm(rd + h));
+	return ret;
 }
-int f(int idx, pll point) { return change(ccw(tovec(point, convex[1][idx]), tovec(point, convex[1][(idx + 1) % m]))); }
-int f2(int idx, pll a, pll b) { return change(ccw(tovec(a, b), tovec(a, convex[0][idx]))); }
-pll findrange(pll point) {
-	int lb = 0, rb = m - 1, lpoint, rpoint;
-	int p = f(0, point);
-	int q = f(m - 1, point);
-	if (!p) {
-		lb = 1;
-		p = f(1, point);
-	}
-	if (!q) {
-		rb = m - 2;
-		q = f(m - 2, point);
-	}
-	if (p != q) {
-		if (p == 1) {
-			rpoint = lb;
-			int l = lb, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) == 1) l = mid + 1;
-				else r = mid;
-			}
-			lpoint = r;
-		}
-		else {
-			if (f(m - 1, point)) lpoint = 0;
-			else lpoint = m - 1;
-			int l = lb, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) <= 0) l = mid + 1;
-				else r = mid;
-			}
-			rpoint = r;
-		}
-	}
-	else {
-		if (p == 1) {
-			int l2 = lb, r2 = rb;
-			while (l2 < r2) {
-				int mid = (l2 + r2) / 2;
-				if (change(ccw(tovec(point, convex[1][0]), tovec(point, convex[1][mid]))) >= 0) l2 = mid + 1;
-				else r2 = mid;
-			}
-			int l = lb, r = r2 - 1;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) == 1) l = mid + 1;
-				else r = mid;
-			}
-			lpoint = r;
-			l = r2 - 1, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) <= 0) l = mid + 1;
-				else r = mid;
-			}
-			rpoint = r;
-		}
-		else {
-			int l2 = lb, r2 = rb;
-			while (l2 < r2) {
-				int mid = (l2 + r2) / 2;
-				if (change(ccw(tovec(point, convex[1][0]), tovec(point, convex[1][mid]))) <= 0) l2 = mid + 1;
-				else r2 = mid;
-			}
-			int l = lb, r = r2 - 1;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) <= 0) l = mid + 1;
-				else r = mid;
-			}
-			rpoint = r;
-			l = r2 - 1, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (f(mid, point) == 1) l = mid + 1;
-				else r = mid;
-			}
-			lpoint = r;
-		}
-	}
-	return { lpoint, rpoint };
+struct Sphere {
+	ll x, y, z, r;
+	Sphere(ll x_ = 0, ll y_ = 0, ll z_ = 0, ll r_ = 0) : x(x_), y(y_), z(z_), r(r_) {}
+	bool operator < (const Sphere& q) const { return r > q.r; }
+	Sphere operator - (const Sphere& q) const { return { x - q.x, y - q.y, z - q.z, 0 }; }
+	ll operator * (const Sphere& q) const { return x * q.x + y * q.y + z * q.z; }
+	ld vol() const { return (4. / 3) * PI * r * r * r; }
+	ld vol(const ld& h) const { return PI * h * h * (3 * r - h) / 3; }
+	ld surf() const { return PI * 4 * r * r; }
+	ld surf(const ld& h) const { return PI * 2 * r * h; }
+} S[3], SP[LEN * 3];
+ll Euc(const Sphere& p, const Sphere& q) { return sq(p.x - q.x) + sq(p.y - q.y) + sq(p.z - q.z); }
+ld mag(const Sphere& p, const Sphere& q) { return sqrtl(Euc(p, q)); }
+ld rad(const Sphere& a, const Sphere& b, const Sphere& c) {
+	ld dab = mag(a, b);
+	ld dac = mag(a, c);
+	ll det = (b - a) * (c - a);
+	ld proj = det / dab;
+	ld ret = fit(proj / dac, -1, 1);
+	return acos(ret);
 }
-int findidx(pll a, pll b, int k) {
-	int lb = 0, rb = n - 1;
-	int p = f2(0, a, b);
-	int q = f2(n - 1, a, b);
-	if (!p) {
-		lb = 1;
-		p = f2(1, a, b);
-	}
-	if (!q) {
-		rb = n - 2;
-		q = f2(n - 2, a, b);
-	}
-	if (p != q) {
-		if (p == 1) {
-			if (k) return (lb - 1 + n) % n;
-			if (!f2(n - 1, a, b)) return n - 1;
-			return 0;
-		}
-		else {
-			int l = lb, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (k) {
-					if (f2(mid, a, b) <= 0) l = mid + 1;
-					else r = mid;
-				}
-				else {
-					if (f2(mid, a, b) < 0) l = mid + 1;
-					else r = mid;
-				}
-			}
-			if (k) return (r - 1 + n) % n;
-			return r;
-		}
-	}
-	else {
-		if (p == 1) {
-			int l2 = lb, r2 = rb;
-			while (l2 < r2) {
-				int mid = (l2 + r2) / 2;
-				if (change(ccw(tovec(convex[0][0], a), tovec(convex[0][0], convex[0][mid]))) < 0 || !mid) l2 = mid + 1;
-				else r2 = mid;
-			}
-			int l = r2, r = rb;
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (k) {
-					if (f2(mid, a, b) <= 0) l = mid + 1;
-					else r = mid;
-				}
-				else {
-					if (f2(mid, a, b) < 0) l = mid + 1;
-					else r = mid;
-				}
-			}
-			if (k) return (r - 1 + n) % n;
-			return r;
-		}
-		else {
-			int l2 = lb, r2 = rb;
-			while (l2 < r2) {
-				int mid = (l2 + r2) / 2;
-				if (change(ccw(tovec(convex[0][0], a), tovec(convex[0][0], convex[0][mid]))) <= 0) l2 = mid + 1;
-				else r2 = mid;
-			}
-			int l = lb, r = r2 - 1;
-			if (f2(r, a, b) == 0) return r;
-			if (f2(r, a, b) < 0) {
-				if (k) return r;
-				return (r + 1) % n;
-			}
-			while (l < r) {
-				int mid = (l + r) / 2;
-				if (k) {
-					if (f2(mid, a, b) <= 0) l = mid + 1;
-					else r = mid;
-				}
-				else {
-					if (f2(mid, a, b) < 0) l = mid + 1;
-					else r = mid;
-				}
-			}
-			if (k) return (r - 1 + n) % n;
-			return r;
-		}
-	}
+int meet(const Sphere& p, const Sphere& q) {
+	ll dist = Euc(p, q);
+	ll rout = sq(p.r + q.r);
+	if (dist >= rout) return OUTSIDE;
+	ll rin = sq(p.r - q.r);
+	if (dist <= rin) return INSIDE;
+	return MEET;
 }
-pdd findcross(pll a, pll b, pll c, pll d) {
-	long double x1 = a.first, y1 = a.second, x2 = b.first, y2 = b.second, x3 = c.first, y3 = c.second, x4 = d.first, y4 = d.second;
-	if (x1 == x2) return { x1, (y4 - y3)* (x1 - x3) / (x4 - x3) + y3 };
-	if (x3 == x4) return { x3, (y2 - y1)* (x3 - x1) / (x2 - x1) + y1 };
-	long double resx = x1 * (x4 - x3) * (y2 - y1) - x3 * (y4 - y3) * (x2 - x1) + y3 * (x2 - x1) * (x4 - x3) - y1 * (x2 - x1) * (x4 - x3);
-	resx /= ((x4 - x3) * (y2 - y1) - (y4 - y3) * (x2 - x1));
-	long double resy = (y2 - y1) * (resx - x1) / (x2 - x1) + y1;
-	return { resx, resy };
+ld cos_2nd(const ld& a, const ld& b, const ld& c) {
+	ld num = sq(a) + sq(b) - sq(c);
+	ld den = 2 * a * b;
+	ld t = num / den;
+	return std::abs(acosl(std::min(std::max(t, -(ld)1.0), (ld)1.0)));
 }
-long double surface(int l, int r, int idx) {
-	if (l < r) return (long double)(area[idx][r] - area[idx][l] - ccw(tovec(convex[idx][0], convex[idx][l]), tovec(convex[idx][0], convex[idx][r])));
-	if (!idx) return (long double)(area[idx][n - 1] + area[idx][r] - area[idx][l] + ccw(tovec(convex[idx][0], convex[idx][r]), tovec(convex[idx][0], convex[idx][l])));
-	return (long double)(area[idx][m - 1] + area[idx][r] - area[idx][l] + ccw(tovec(convex[idx][0], convex[idx][r]), tovec(convex[idx][0], convex[idx][l])));
+void spherical_triangle_angles(const ld& a, const ld& b, const ld& c, ld& A_, ld& B_, ld& C_) {
+	A_ = acos(fit((cos(a) - cos(b) * cos(c)) / (sin(b) * sin(c)), -1, 1));
+	B_ = acos(fit((cos(b) - cos(a) * cos(c)) / (sin(a) * sin(c)), -1, 1));
+	C_ = acos(fit((cos(c) - cos(a) * cos(b)) / (sin(a) * sin(b)), -1, 1));
+	return;
 }
-int main(void) {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> n >> m >> q;
-	long long int x, y;
-	for (int i = 0; i < n; i++) {
-		cin >> x >> y;
-		convex[0].push_back({ x, y });
-		if (i >= 2) area[0][i] = area[0][i - 1] + ccw(tovec(convex[0][0], convex[0][i - 1]), tovec(convex[0][0], convex[0][i]));
+ld area(const ld& a, const ld& b, const ld& c, const ll& r, const ld& t) {
+	ld A_, B_, C_;
+	if (a >= PI) {
+		spherical_triangle_angles(a * .5, t, c, A_, B_, C_);
+		return r * r * (A_ + B_ + C_ - PI) * 2;
 	}
-	for (int i = 0; i < m; i++) {
-		cin >> x >> y;
-		convex[1].push_back({ x, y });
-		if (i >= 2) area[1][i] = area[1][i - 1] + ccw(tovec(convex[1][0], convex[1][i - 1]), tovec(convex[1][0], convex[1][i]));
-	}
-	for (int i = 0; i < q; i++) {
-		cin >> x >> y;
-		if (inconvex(0, { x, y }) <= 0) {
-			cout << "OUT\n";
-			continue;
-		}
-		if (inconvex(1, { x, y }) >= 0) {
-			cout << "IN\n";
-			continue;
-		}
-		pll range = findrange({ x, y });
-		int crossright = findidx({ x, y }, convex[1][range.second], 1);
-		int crossleft = findidx({ x, y }, convex[1][range.first], 0);
-		pdd lcross = findcross({ x, y }, convex[1][range.first], convex[0][crossleft], convex[0][(crossleft - 1 + n) % n]);
-		pdd rcross = findcross({ x, y }, convex[1][range.second], convex[0][crossright], convex[0][(crossright + 1) % n]);
-		long double res = surface(crossleft, crossright, 0) + dccw(dtovec(convex[0][crossleft], convex[0][crossright]), dtovec(convex[0][crossleft], rcross)) + dccw(dtovec(convex[0][crossleft], rcross), dtovec(convex[0][crossleft], lcross));
-		res -= surface(range.first, range.second, 1) + dccw(dtovec(convex[1][range.first], convex[1][range.second]), dtovec(convex[1][range.first], rcross)) + dccw(dtovec(convex[1][range.first], rcross), dtovec(convex[1][range.first], lcross));
-		cout << fixed << setprecision(7);
-		cout << (area[0][n - 1] - res - area[1][m - 1]) / 2 << '\n';
-	}
+	spherical_triangle_angles(a, b, c, A_, B_, C_);
+	return r * r * (A_ + B_ + C_ - PI);
 }
-
-/*
-
-5 4 1
--2 -2
-1 -2
-3 0
-1 4
--2 4
-0 0
-1 0
-1 1
-0 1
-2 0
-
-3 3 1
-0 0
-10 0
-5 10
-4 4
-6 4
-5 5
-5 1
-
-4 4 1
--4 -4
-4 -4
-4 4
--4 4
--1 -3
-1 -3
-1 -2
--1 -2
-0 0
-
-4 4 1
--100 -100
-100 -100
-100 100
--100 100
--10 10
--10 -10
-10 -10
-10 10
-0 -50
-
-*/
+void query(const int& q) {
+	for (int i = 0; i < 3; i++) {
+		std::cin >> S[i].x >> S[i].y >> S[i].z >> S[i].r, F[i] = 0;
+		SP[q * 3 + i] = S[i];
+	}
+	std::sort(S, S + 3);
+	assert(S[0].r >= S[1].r && S[1].r >= S[2].r);
+	return;
+}
+//#define READ_FILE
+//#define AUTO_CHECK
+void solve() {
+	std::cin.tie(0)->sync_with_stdio(0);
+	std::cout.tie(0);
+	std::cout << std::fixed;
+	std::cout.precision(15);
+#ifdef READ_FILE
+	freopen("../../../input_data/e/e000.in", "r", stdin);
+	freopen("../../../input_data/e/ret.txt", "w", stdout);
+#endif
+	std::cin >> T; Q.resize(T); sts.resize(T);
+	for (int q = 0; q < T; q++) query(q);
+	return;
+}
+int main() { solve(); return 0; }//boj23590

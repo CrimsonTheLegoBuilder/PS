e81310e2254fc371f62d53f4a9a09bd122c4391a
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-01-29 23:52:57 +0900

    java 15

--- PS_java/src/boj_bronze.java
+++ PS_java/src/boj_bronze.java
@@ -1,36 +1,72 @@
-//import java.io.BufferedWriter;
-//import java.io.OutputStreamWriter;
-//import java.io.IOException;
-//import java.util.Arrays;
+import java.io.*;
 import java.util.Scanner;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class boj_bronze {
 //public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
-        for (int t = 1; t <= T; t++) {
-            int r = sc.nextInt();
-            int a = sc.nextInt();
-            int b = sc.nextInt();
-            double total = r * r * Math.PI;
-            int cur = r;
-            long cnt = 0;
-            while (cur > 0) {
-                if (cnt % 2 != 1) cur *= a;
-                else cur /= b;
-                //System.out.println("DEBUG[" + t + "]:: cur:: " + cur);
-                cnt++;
-                total += cur * cur * Math.PI;
-            }
-            System.out.print("Case #" + t + ": ");
-            System.out.printf("%.6f\n", total);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        long[][] P = new long[N][2];
+        long[][] Q = new long[M][2];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            P[i][0] = Long.parseLong(st.nextToken());
+            P[i][1] = Long.parseLong(st.nextToken());
         }
+
+        for (int j = 0; j < M; j++) {
+            st = new StringTokenizer(br.readLine());
+            Q[j][0] = Long.parseLong(st.nextToken());
+            Q[j][1] = Long.parseLong(st.nextToken());
+        }
+
+        long ans = -1;
+
+        for (int j = 0; j < M; j++) {
+            for (int i = 0; i < N; i++) {
+                long x = Q[j][0] - P[i][0];
+                long y = Q[j][1] - P[i][1];
+                ans = Math.max(ans, x * x + y * y);
+            }
+        }
+
+        //String ret = String.format("%.7f", d);
+        String ret = String.valueOf(ans);
+        bw.write(ret);
+        bw.newLine();
+        bw.flush();
+        bw.close();
     }
 }
 
 /*
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        for (int t = 1; t <= T; t++) {
+            int r = sc.nextInt();
+            int b = sc.nextInt();
+            double s = (double)r * Math.sqrt(2);
+            double ans = s * s;
+            if (b < s) {
+                int r2 = r * 2;
+                double d = Math.sqrt(r2 * r2 - b * b);
+                ans = d * b;
+            }
+            System.out.printf("%.3f\n", ans);
+        }
+    }
+
             String s = scan.next();
             int len = s.length();
             char l = s.charAt(len / 2 - 1);

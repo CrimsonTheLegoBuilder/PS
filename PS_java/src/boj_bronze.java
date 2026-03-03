1e2de17832cfeb671e76f986cf92dd7effc08543
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-03-03 23:37:35 +0900

    java 38

--- PS_java/src/boj_bronze.java
+++ PS_java/src/boj_bronze.java
@@ -15,24 +15,11 @@ public class boj_bronze {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         //StringTokenizer st = new StringTokenizer(br.readLine());
         StringTokenizer st;
-        while (true) {
-            int[] D = new int[12];
-            int f = 0;
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < 12; i++) {
-                D[i] = Integer.parseInt(st.nextToken());
-                if (D[i] == 0) f++;
-            }
-            if (f == 12) break;
-            Arrays.sort(D);
-            f = 0;
-            if (D[0] == D[3]) f++;
-            if (D[4] == D[7]) f++;
-            if (D[8] == D[11]) f++;
-            if (f == 3) bw.write("yes\n");
-            else bw.write("no\n");
-        }
-
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        double ans = 6 * Math.sqrt(2. * N / 3 / Math.sqrt(3));
+        String ret = String.format("%.10f", ans);
+        bw.write(ret + "\n");
         //String ret = String.format("%.10f", d);
         //String ret = String.valueOf(ans);
         //bw.write("\n");

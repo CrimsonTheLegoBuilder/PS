950e1241bd211321ea2cdd4c8e3739e66abb94be
CrimsonTheLegoBuilder <redcube231@gmail.com>
2026-04-01 20:04:51 +0900

    ts 02

--- ts_test/src/main.ts
+++ ts_test/src/main.ts
@@ -4,24 +4,19 @@ const input = fs.readFileSync(0, 'utf8');//표준 입력 읽기
 const lines = input.split(/\s+/);//입력 줄을 공백 등 기준으로 다 잘라서 배열로 저장
 
 let cur = 0;
-while (cur < lines.length) {
-    const nStr = lines[cur++];//하나 읽고 커서 내림
-    if (!nStr) break;
 
-    const N = parseInt(nStr);
-    if (N === -1) break;
+const N = parseInt(lines[0] || "0");
+let num: number[] = [];
+let ret = new Array(N + 1).fill(0);
 
-    let totalDistance = 0;
-    let previousTime = 0;
+for (let i = 1; i <= N; i++) {
+    const n = parseInt(lines[i]);
+    ret[n] = 1;
+}
 
-    for (let i = 0; i < N; i++) {
-        const s = parseInt(lines[cur++] || "0");//속도 읽기
-        const t = parseInt(lines[cur++] || "0");//시간 읽기
-
-        const duration = t - previousTime;//실제 주행 시간
-        totalDistance += s * duration;
-        previousTime = t;
+for (let i = 1; i <= N; i++) {
+    if (ret[i] === 0) {
+        console.log(i);
     }
 
-    process.stdout.write(totalDistance + " miles\n");
 }
